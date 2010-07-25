626ceadf0a4aa0f71cc926c809a23fad379d3b36
gideonz <gideonz@localhost>
2010-07-25 15:13:13 +0000

    More missing files.. Had a little NFS problem.. sorry!

---
+++ software/io/icap/icap.h
@@ -0,0 +1,8 @@
+#ifndef ICAP_H
+#define ICAP_H
+
+#define ICAP_FPGA_TYPE *((volatile BYTE *)0x4060600)
+#define ICAP_PULSE     *((volatile BYTE *)0x4060604)
+#define ICAP_WRITE     *((volatile BYTE *)0x4060608)
+
+#endif
