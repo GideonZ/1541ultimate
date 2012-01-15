53bed658b80a4546cedbcfe94a43af3359fd7c57
gideonz <gideonz@localhost>
2012-01-15 23:05:38 +0000

    Going for a clean build of 2.4c

--- software/application/versions.h
+++ software/application/versions.h
@@ -1,9 +1,11 @@
 #ifndef VERSIONS_H
 #define VERSIONS_H
 
-#define APPL_VERSION "2.4"
+#define APPL_VERSION "2.4c"
 #define BOOT_VERSION "V1.3"
 #define FPGA_VERSION "FPGA U2 VAC"
 #define MINIMUM_FPGA_REV 0xAC
 
 #endif
+
+/* note: 'c' is to avoid confusion with 'b' of beta. */
