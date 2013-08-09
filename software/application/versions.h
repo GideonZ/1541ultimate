78a1cae30ebe27161476876b0678e3f6a1faa03a
gideonz <gideonz@localhost>
2013-08-09 12:02:22 +0000

    SID player update, as received from Freshness

--- software/application/versions.h
+++ software/application/versions.h
@@ -1,7 +1,7 @@
 #ifndef VERSIONS_H
 #define VERSIONS_H
 
-#define APPL_VERSION "2.6g"
+#define APPL_VERSION "2.6h"
 #define BOOT_VERSION "V1.4"
 #define FPGA_VERSION "FPGA U2 VF6 "
 #define MINIMUM_FPGA_REV 0xF2
