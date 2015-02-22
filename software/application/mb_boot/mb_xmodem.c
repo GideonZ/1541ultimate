ee18d836bffe65d13b9c57bbda6a97c8a84da3a4
gideonz <gideonz@localhost>
2015-02-22 21:54:30 +0000

    Made separate boot target for xmodem downloads. Added 3-hit error retry mechanism in USB nano code.

---
+++ software/application/mb_boot/mb_xmodem.c
@@ -0,0 +1,36 @@
+#include "small_printf.h"
+#include "itu.h"
+#include "xmodem.h"
+
+void (*function)();
+
+void jump_run(DWORD a)
+{
+    DWORD *dp = (DWORD *)&function;
+    *dp = a;
+    function();
+}
+
+int main(void)
+{
+	puts("Hello, world!");
+
+    BYTE *ram = (BYTE *)0x10000;
+
+    while(1) {
+    	int st = xmodemReceive(ram, 1048576);
+        if (st > 0) {
+            puts("Receive done.");
+            puts("Going to start...");
+            wait_ms(2000);
+            //jump_run(0x10000);
+            UART_DATA = 0x2d;
+            asm("bralid r15, 0x10000");
+            asm("nop");
+            puts("Application exit.");
+        } else {
+            puts("Nothing received.");
+        }
+    }    
+    return 0;
+}
