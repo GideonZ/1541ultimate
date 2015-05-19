a987dcbc2eda95d9960cfe45d0db47e2c48b100f
gideonz <gideonz@localhost>
2015-05-19 05:10:41 +0000

    Work in progress on screen output

--- software/io/c64/screen.cc
+++ software/io/c64/screen.cc
@@ -1,274 +1,50 @@
 #include "screen.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/*
 #include "integer.h"
 
 extern "C" {
     #include "itu.h"
 	#include "small_printf.h"
 }
+*/
 
-Screen :: Screen(char *b, char *c, int sx, int sy)
-{
-    root_base       = b;
-    root_base_col   = c;
-    window_base     = b;
-    window_base_col = c;
-    size_x     = sx;
-    size_y     = sy;
-    window_x   = sx; // default to full screen
-    window_y   = sy;
-    offset_x   = 0;
-    offset_y   = 0;
-    cursor_x   = 0;
-    cursor_y   = 0;
-    pointer    = 0;
-    color      = 15;
-    border_h   = 0;
-    border_v   = 0;
-    reverse    = 0;
-    backup_chars = NULL;
-    backup_color = NULL;
-    allow_scroll = true;
-    escape       = false;
-    escape_cmd	 = false;
-    escape_cmd_pos = 0;
-    parent       = NULL;
-    cursor_on	 = 0;
-    backup();
-}
-
-Screen :: Screen(Screen *scr, int x1, int y1, int sx, int sy)
-{
-	x1 += scr->border_v;
-	y1 += scr->border_h;
-
-    root_base       = scr->root_base;
-    root_base_col   = scr->root_base_col;
-    window_base     = scr->root_base+(y1 * scr->size_x)+x1;
-    window_base_col = scr->root_base_col+(y1 * scr->size_x)+x1;
-//    printf("Window Created: %p %p\n", window_base, window_base_col);
-    size_x     = scr->size_x;
-    size_y     = scr->size_y;
-    window_x   = sx;
-    window_y   = sy;
-    offset_x   = x1;
-    offset_y   = y1;
-    cursor_x   = 0;
-    cursor_y   = 0;
-    pointer    = 0;
-    color      = 15;
-    border_h   = 0;
-    border_v   = 0;
-    reverse    = 0;
-    backup_chars = NULL;
-    backup_color = NULL;
-    allow_scroll = true;
-    escape       = false;
-    escape_cmd	 = false;
-    escape_cmd_pos = 0;
-    parent       = scr;
-    cursor_on	 = 0;
-    backup();
-}
-
-Screen :: ~Screen()
-{
-    restore();
-}
-
-void Screen :: dump(void)
-{
-    printf("Screen %p (window_base: %p) %d/%d %d/%d [%d/%d]\n", this, window_base, window_x, window_y, offset_x, offset_y, border_v, border_h);
-    if(parent)
-        parent->dump();
-}
-
-void Screen :: backup(void)
-{
-    backup_chars = new char[window_x * window_y];
-    backup_color = new char[window_x * window_y];
-    
-    char *b = window_base;
-    char *c = window_base_col;
-    int i=0;
-    for(int y=0;y<window_y;y++) {
-        for(int x=0;x<window_x;x++) {
-            backup_chars[i] = b[x];
-            backup_color[i++] = c[x];
-            b[x] = 0;
-            c[x] = (char)color;
-        }
-        b+=size_x;
-        c+=size_x;
-    }
-    cursor_x = cursor_y = 0;
-    pointer = 0; //(offset_y * size_y) + offset_x;
-}
-
-void Screen :: restore(void)
-{
-    if (!backup_chars)
-        return;
-    if (!backup_color)
-        return;
-
-    while(border_h > 0) {
-        border_h--;
-        window_base -= size_x;
-        window_base_col -= size_x;
-        window_y  += 2;
-    }
-
-    while(border_v > 0) {
-        border_v--;
-        window_base -= 1;
-        window_base_col -= 1;
-        window_x  += 2;
-    }
-    
-    char *b = window_base;
-    char *c = window_base_col;
-
-    int i=0;
-    for(int y=0;y<window_y;y++) {
-        for(int x=0;x<window_x;x++) {
-            b[x] = backup_chars[i];
-            c[x] = backup_color[i++];
-        }
-        b+=size_x;
-        c+=size_x;
-    }
-    delete[] backup_chars;
-    delete[] backup_color;    
-
-    backup_chars = NULL;
-    backup_color = NULL;
-//    printf("Window destructed.\n");
-    
-}
-   
-
-void Screen :: set_color(int c)
-{
-    color = c;
-}
-    
-void Screen :: clear(void)
-{
-    char *b = window_base;
-    char *c = window_base_col;
-    for(int y=0;y<window_y;y++) {
-        for(int x=0;x<window_x;x++) {
-            b[x] = 0;
-            c[x] = (char)color;
-        }
-        b+=size_x;
-        c+=size_x;
-    }
-    cursor_x = cursor_y = 0;
-    pointer = 0; //(offset_y * size_y) + offset_x;
-    allow_scroll = true;
-    reverse = 0;
-}
-
-void Screen :: no_scroll(void)
-{
-    allow_scroll = false;
-}
-    
-    
-void Screen :: move_cursor(int x, int y)
-{
-	if(cursor_on)
-		window_base[pointer] ^= 0x80;
-
-	if(x >= window_x)
-        x = window_x-1;
-    if(y >= window_y)
-        y = window_y-1;
-    if(x < 0)
-        x = 0;
-    if(y < 0)
-        y = 0;
-        
-    cursor_x = x;
-    cursor_y = y;
-    pointer = (y * size_x) + x;
-
-	if(cursor_on)
-		window_base[pointer] ^= 0x80;
-}
-
-void Screen :: scroll_up()
-{
-    char *b = window_base;
-    char *c = window_base_col;
-    for(int y=0;y<window_y-1;y++) {
-        for(int x=0;x<window_x;x++) {
-            b[x] = b[x+size_x];
-            c[x] = c[x+size_x];
-        }
-        b+=size_x;
-        c+=size_x;
-    }
-    for(int x=0;x<window_x;x++)
-        b[x] = 0;
-}
-
-/*void Screen :: scroll_down()
-{
-    char *b = &window_base[(window_y-2)*size_x];
-    char *c = &window_base_col[(window_y-2)*size_x];
-    for(int y=0;y<window_y-1;y++) {
-        for(int x=0;x<window_x;x++) {
-            b[x+size_x] = b[x];
-            c[x+size_x] = c[x];
-        }
-        b-=size_x;
-        c-=size_x;
-    }
-    for(int x=0;x<window_x;x++)
-        b[x+size_x] = 0;
-}*/
-
-void Screen :: reverse_mode(int r)
-{
-    reverse = r;
-}
-    
 const short bright_map[16] = { 0, 1, 10, 3, 10, 13, 14, 7, 7, 8, 10, 15, 15, 13, 14,  1 };
 const short dim_map[16]    = { 0, 15, 2, 3,  4,  5,  6, 8, 9, 9,  2, 11, 11,  5,  6, 15 };
 
-void Screen :: doAttr(int val)
+void VT100_Parser :: doAttr(int val)
 {
 	switch(val) {
 	case 0:
-		color = 15; // should return to foreground color TODO
-		reverse = 0;
+		owner->set_color(15);
+		owner->reverse_mode(0);
 		break;
 	case 1:
-		color = bright_map[color & 15];
+		owner->set_color(bright_map[owner->get_color() & 15]);
 		break;
 	case 2:
-		color = dim_map[color & 15];
+		owner->set_color(dim_map[owner->get_color() & 15]);
 		break;
 	case 7:
-		reverse = 1;
+		owner->reverse_mode(1);
 		break;
-	case 30: color = 0; break;
-	case 31: color = 2; break;
-	case 32: color = 5; break;
-	case 33: color = 7; break;
-	case 34: color = 6; break;
-	case 35: color = 4; break;
-	case 36: color = 3; break;
-	case 37: color = 1; break;
+	case 30: owner->set_color(0); break;
+	case 31: owner->set_color(2); break;
+	case 32: owner->set_color(5); break;
+	case 33: owner->set_color(7); break;
+	case 34: owner->set_color(6); break;
+	case 35: owner->set_color(4); break;
+	case 36: owner->set_color(3); break;
+	case 37: owner->set_color(1); break;
 	default:
 		printf("Unhandled VT100 attribute %d\n", val);
 		break;
 	}
 }
 
-void Screen :: parseAttr()
+void VT100_Parser :: parseAttr()
 {
 	int value = 0;
 	for(int i=0;i<escape_cmd_pos;i++) {
@@ -284,14 +60,13 @@ void Screen :: parseAttr()
 	doAttr(value);
 }
 
-
-void Screen :: output(char c)
+int VT100_Parser :: processChar(char c)
 {
 	if(escape) {
 		if ((!escape_cmd) && (c == '[')) {
 			escape_cmd_pos = 0;
 			escape_cmd = true;
-			return;
+			return 0;
 		}
 
 		if (((c >= 'A')&&(c <= 'Z')) || ((c >= 'a')&&(c <= 'z')) || (escape_cmd_pos == 15)) {
@@ -303,16 +78,126 @@ void Screen :: output(char c)
 		} else {
 			escape_cmd_data[escape_cmd_pos++] = c;
 		}
-		return;
+		return 0;
+	} else if(c == 0x1B) {
+		escape = true;
+		return 0;
 	}
-		
+	owner->output_raw(c);
+	return 1;
+}
+
+/**
+ * Basic screen implementation, assuming a memory mapped character grid and a memory mapped attribute grid
+ */
+Screen :: Screen(char *b, char *c, int sx, int sy)
+{
+	term = 0;
+
+	char_base  = b;
+    color_base = c;
+
+    size_x     = sx;
+    size_y     = sy;
+
+    color      = 15;
+    reverse    = 0;
+    allow_scroll = true;
+    cursor_on	 = 0;
+
+    cursor_x = 0;
+    cursor_y = 0;
+    pointer = 0;
+}
+
+Screen :: ~Screen()
+{
+	if (term)
+		delete term;
+}
+
+void Screen :: enableVT100Parsing()
+{
+	term = new VT100_Parser(this);
+}
+
+void Screen :: move_cursor(int x, int y)
+{
+	if(cursor_on)
+		char_base[pointer] ^= 0x80;
+
+	cursor_x = x;
+	cursor_y = y;
+	pointer = (y * size_x) + x;
+
+	if(cursor_on)
+		char_base[pointer] ^= 0x80;
+
+}
+
+void Screen :: scroll_up()
+{
+    char *b = char_base;
+    char *c = color_base;
+    for(int y=0;y<size_y-1;y++) {
+        for(int x=0;x<size_x;x++) {
+            b[x] = b[x+size_x];
+            c[x] = c[x+size_x];
+        }
+        b+=size_x;
+        c+=size_x;
+    }
+    for(int x=0;x<size_x;x++)
+        b[x] = 0;
+}
+
+void Screen :: scroll_down()
+{
+    char *b = &char_base[(size_y-2)*size_x];
+    char *c = &color_base[(size_y-2)*size_x];
+    for(int y=0;y<size_y-1;y++) {
+        for(int x=0;x<size_x;x++) {
+            b[x+size_x] = b[x];
+            c[x+size_x] = c[x];
+        }
+        b-=size_x;
+        c-=size_x;
+    }
+    for(int x=0;x<size_x;x++)
+        b[x+size_x] = 0;
+}
+
+void Screen :: repeat(char a, int len)
+{
+	if(cursor_on)
+		char_base[pointer] ^= 0x80;
+
+	for(int i=0;i<len;i++) {
+		char_base[pointer++] = a;
+	}
+
+	if(cursor_on)
+		char_base[pointer] ^= 0x80;
+}
+
+int  Screen :: output(char c)
+{
+	int h = (!term) ? 1 : (term ->processChar(c));
+	if (h) {
+		output_raw(c);
+	}
+	return h;
+}
+
+void Screen :: output_raw(char c)
+{
 	if (cursor_on) {
-		char *p = window_base + pointer;
-		*p ^= 0x80; // remove cursor
+		char *p = char_base + pointer;
+		*p ^= 0x80; // unplace cursor
 	}
 	switch(c) {
         case 0x0A:
-            if(cursor_y == window_y-1) {
+            if(cursor_y == size_y-1) {
                 if(allow_scroll)
                     scroll_up();
             } else {
@@ -327,25 +212,30 @@ void Screen :: output(char c)
             cursor_x = 0;
             break;
         case 0x08:
-            // todo: backspace
+            if (pointer > 0) {
+            	pointer --;
+            	cursor_x --;
+            	if (cursor_x < 0) {
+            		cursor_x = size_x - 1;
+            		cursor_y --;
+            	}
+            	char_base[pointer] = 0;
+            }
             break;
-        case 0x1B: // escape
-        	escape = true;
-        	break;
         default:
             if(reverse)
-                window_base[pointer] = c | 0x80;
+                char_base[pointer] = c | 0x80;
             else
-                window_base[pointer] = c;
-                
-            window_base_col[pointer] = (char)color;
+                char_base[pointer] = c;
+
+            color_base[pointer] = (char)color;
             pointer ++;
             cursor_x++;
-        
-            if(cursor_x == window_x) {
+
+            if(cursor_x == size_x) {
                 cursor_x = 0;
-                pointer -= window_x;
-                if(cursor_y == window_y-1) {
+                pointer -= size_x;
+                if(cursor_y == size_y-1) {
                     if(allow_scroll)
                         scroll_up();
                 } else {
@@ -355,165 +245,185 @@ void Screen :: output(char c)
             }
     }
 	if (cursor_on) {
-		char *p = window_base + pointer;
+		char *p = char_base + pointer;
 		*p ^= 0x80; // place cursor
 	}
 }
+
+void Screen :: output_fixed_length(char *string, int offset_x, int width)
+{
+	if (cursor_on) {
+		char *p = char_base + pointer;
+		*p ^= 0x80; // unplace cursor
+	}
+	pointer = (cursor_y * size_x) + offset_x;
+	cursor_x = offset_x;
+
+	char c;
+	int chars_placed = 0;
+    for(int i=0;i<width;i++,string++) {
+        c = *string;
+        if(!c) {
+        	repeat(0, width - chars_placed);
+			break;
+        }
+
+        chars_placed += output(c);
+    }
+	if (cursor_on) {
+		char *p = char_base + pointer;
+		*p ^= 0x80; // place cursor
+	}
+}
+
+/**
+ * A simple window overlay, that maintains the offset relative to a screen position, and is capable of
+ * drawing a border.
+ */
+Window :: Window(Screen *parent, int x1, int y1, int sx, int sy)
+{
+	window_x   = sx; // default to full screen
+    window_y   = sy;
+    offset_x   = 0;
+    offset_y   = 0;
+    cursor_x   = 0;
+    cursor_y   = 0;
+    border_h   = 0;
+    border_v   = 0;
+    this->parent = parent;
+}
+
+Window :: ~Window()
+{
+}
+
+void Window :: set_color(int c)
+{
+    parent->set_color(c);
+}
+
+void Window :: no_scroll(void)
+{
+    parent->scroll_mode(false);
+}
+
+void Window :: move_cursor(int x, int y)
+{
+	if(x >= window_x)
+        x = window_x-1;
+    if(y >= window_y)
+        y = window_y-1;
+    if(x < 0)
+        x = 0;
+    if(y < 0)
+        y = 0;
+        
+    cursor_x = x;
+    cursor_y = y;
+
+    parent->move_cursor(x + offset_x, y + offset_y);
+}
+
+void Window :: clear(void)
+{
+    for(int y=0;y<window_y;y++) {
+    	parent->move_cursor(offset_x, offset_y+y);
+    	parent->repeat(' ', window_x);
+    }
+    cursor_x = cursor_y = 0;
+    parent->reverse_mode(0);
+    parent->scroll_mode(true);
+    parent->move_cursor(offset_x, offset_y);
+}
+
+
+void Window :: reverse_mode(int r)
+{
+	parent->reverse_mode(r);
+}
+
     
-void Screen :: output(char *string)
+void Window :: output(char *string)
 {
     while(*string) {
         output(*(string++));
     }
 }
 
-void Screen :: output_length(char *string, int len)
+void Window :: repeat(char a, int len)
 {
-	if(cursor_on)
-		window_base[pointer] ^= 0x80;
-
-	int temp = pointer - cursor_x;
-    char c;
-    int i;
-    for(i=0;i<window_x;string++) {
-        c = *string;
-        if(i == len) {
-			for(int j=i;j<window_x;j++)
-				window_base[temp++] = 0;
-			break;
-		}
-        window_base[temp] = c;
-        window_base_col[temp] = (char)color;
-        temp++;
-        i++;
-    }
-    pointer = temp;
-    cursor_x = i;
-
-	if(cursor_on)
-		window_base[pointer] ^= 0x80;
+	parent->repeat(a, len);
 }
 
-void Screen :: repeat(char a, int len)
+
+void Window :: output_line(char *string)
 {
-	if(cursor_on)
-		window_base[pointer] ^= 0x80;
-
-	for(int i=0;i<len;i++) {
-		window_base[pointer++] = a;
-	}
-
-	if(cursor_on)
-		window_base[pointer] ^= 0x80;
+	parent->output_fixed_length(string, offset_x, window_x);
 }
 
-void Screen :: output_line(char *string)
+void Window :: output_length(char *string, int len)
 {
-    int temp = pointer - cursor_x;
-    int col  = color;
-    char c;
-    for(int i=0;i<window_x;string++) {
-        c = *string;
-        if(!c) {
-			for(int j=i;j<window_x;j++)
-				window_base[temp++] = 0;
-			break;
-		}
-		
-        if((c >= 0x10)&&(c <= 0x1F)) {
-            col = int(c - 0x10);
-            continue;
-        }
-        if(reverse)
-            window_base[temp] = c | 0x80;
-        else
-            window_base[temp] = c;
-            
-        window_base_col[temp] = (char)col;
-        temp++;
-        i++;
-    }
+	parent->output_fixed_length(string, offset_x, len);
 }
-    
-void Screen :: draw_border(void)
+
+void Window :: draw_border(void)
 {
-    char *scr = window_base;
-    int w = window_x;
-    int h = window_y;
+    parent->move_cursor(offset_x, offset_y);
+    parent->output(1);
+    parent->repeat(2, window_x-2);
+    parent->output(3);
     
-    // draw box from left to right
-    scr[0] = 1; // upper left corner
-    for(int b=1;b<w-1;b++) {
-        scr[b] = 2; // horizontal line
+    parent->move_cursor(offset_x, offset_y + window_y - 1);
+    parent->output(5);
+    parent->repeat(2, window_x-2);
+    parent->output(6);
+
+    for(int i=1;i<window_y-1;i++) {
+    	parent->move_cursor(offset_x, offset_y + i);
+    	parent->output(4);
+    	parent->move_cursor(offset_x + window_x - 1, offset_y + i);
+    	parent->output(4);
     }
-    scr[w-1] = 3; // upper right corner
-    for(int b=1;b<h-1;b++) {
-        scr += size_x;
-        scr[0] = 4;   // vertical bar
-        scr[w-1] = 4; // vertical bar
-    }
-    scr += size_x;
-    scr[0] = 5; // lower left corner
-    for(int b=1;b<w-1;b++) {
-        scr[b] = 2; // horizontal line
-    }
-    scr[w-1] = 6; // lower right corner
-    
-    window_base += size_x + 1;
-    window_base_col += size_x + 1;
+
     window_x  -= 2;
     window_y  -= 2;
     cursor_x   = 0;
     cursor_y   = 0;
-    pointer    = 0;
     border_h ++;
     border_v ++;
 }
 
-void Screen :: draw_border_horiz(void)
+void Window :: draw_border_horiz(void)
 {
-    char *scr = window_base;
-    int w = window_x;
-    int h = window_y;
+    parent->move_cursor(offset_x, offset_y);
+    parent->repeat(2, window_x);
+    parent->move_cursor(offset_x, offset_y + window_y-1);
+    parent->repeat(2, window_x);
     
-    // draw box from left to right
-    for(int b=0;b<w;b++) {
-        scr[b] = 2; // horizontal line
-    }
-    scr += size_x * (h-1);
-    for(int b=0;b<w;b++) {
-        scr[b] = 2; // horizontal line
-    }
-    
-    window_base += size_x;
-    window_base_col += size_x;
-    window_y  -= 2;
-    cursor_x   = 0;
-    cursor_y   = 0;
-    pointer    = 0;
+    offset_y ++;
+    window_y -=2;
+    cursor_x = cursor_y = 0;
     border_h ++;
 }
 
-int Screen :: get_size_x(void)
+int Window :: get_size_x(void)
 {
     return window_x;
 }
 
-int Screen :: get_size_y(void)
+int Window :: get_size_y(void)
 {
     return window_y;
 }
 
-void Screen :: set_char(int x, int y, char c)
+void Window :: set_char(int x, int y, char c)
 {
-    char *scr = window_base;
-    scr -= border_v;
-    scr -= (size_x * border_h);
-    scr += (y * size_x) + x;
-    *scr = c;        
+	parent->move_cursor(x + offset_x, y + offset_y);
+	parent->output(c);
 }
     
-int console_print(Screen *screen, const char *fmt, ...)
+/*
+int console_print(Window *w, const char *fmt, ...)
 {
 	static char str[256];
 
@@ -522,13 +432,14 @@ int console_print(Screen *screen, const char *fmt, ...)
 	char *pnt = str;
 	
     va_start(ap, fmt);
-	if(screen) {
+	if(w) {
 	    ret = _vprintf(_string_write_char, (void **)&pnt, fmt, ap);
 	    _string_write_char(0, (void **)&pnt);
-	    screen->output(str);
+	    w->output(str);
 	} else {
 		ret = _vprintf(_diag_write_char, (void **)&pnt, fmt, ap);
 	}
     va_end(ap);
     return (ret);
 }
+*/
--- software/io/c64/screen.h
+++ software/io/c64/screen.h
@@ -1,61 +1,109 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-class Screen
+class Screen;
+
+class VT100_Parser // can be used / reused for C64 screen, VDC or other character grid screen
 {
-    char *root_base;
-    char *root_base_col;
-    char *window_base;
-    char *window_base_col;
-
-    char *backup_chars;
-    char *backup_color;
-    void backup(void);
-    void restore(void);
-
-protected:
-    char escape_cmd_data[16];
-    Screen *parent;
-    bool allow_scroll;
+    // processing state
+	char escape_cmd_data[16];
     bool escape;
     bool escape_cmd;
     int  escape_cmd_pos;
-    int cursor_on;
-    int size_x;
-    int size_y;
-    int window_x;
-    int window_y;
-    int offset_x;
-    int offset_y;
+
+    Screen *owner; // it will call the functions to set color / mode on this object
+
+    void  parseAttr();
+    void  doAttr(int);
+public:
+    VT100_Parser(Screen *owner) : owner(owner) {
+    	escape = false;
+    	escape_cmd = false;
+    	escape_cmd_pos = 0;
+    }
+    int processChar(char c); // returns 1 when char is printed, 0 otherwise.
+};
+
+class Screen
+{
+	VT100_Parser *term;
+	char *char_base;
+    char *color_base;
+
+	int size_x;
+	int size_y;
+
+    // current position
     int cursor_x;
-    int cursor_y;
-    int pointer;
+	int cursor_y;
+	int pointer;
+
+	friend class VT100_Parser;
+	void output_raw(char c);
+
+	// private stuff
+	void scroll_up(void);
+    void scroll_down(void);
+
+protected:
+    // draw mode
     int color;
-    int border_h;
-    int border_v;
     int reverse;
+    int cursor_on;
+    bool allow_scroll;
 
 public:
     Screen(char *, char *, int, int);
-    Screen(Screen*, int, int, int, int);
     virtual ~Screen();
+    void enableVT100Parsing();
 
+// functions called directly, or from a window
     void  cursor_visible(int a) {
     	if (cursor_on != a) {
-    		char *p = window_base + pointer;
+    		char *p = char_base + pointer;
     		*p ^= 0x80; // remove or place cursor
     	}
     	cursor_on = a;
     }
-    virtual void  parseAttr();
-    virtual void  doAttr(int);
+    virtual void set_color(int c) {color=c;}
+    virtual int  get_color() { return color; }
+    virtual void reverse_mode(int r) {reverse=r;}
+    virtual void scroll_mode(bool b) {allow_scroll=b;}
+
+    // draw functions
+    virtual void move_cursor(int x, int y);
+    virtual int  output(char c);
+    virtual void repeat(char c, int rep);
+    virtual void output_fixed_length(char *string, int offset_x, int width);
+};
+
+class Window
+{
+    Screen *parent;
+
+    // limits
+    int window_x;
+    int window_y;
+    int offset_x;
+    int offset_y;
+    int border_h;
+    int border_v;
+
+    // current position
+    int cursor_x;
+    int cursor_y;
+
+public:
+    Window(Screen*, int, int, int, int);
+    virtual ~Window();
+
     virtual void  set_color(int);
+    virtual void  reverse_mode(int);
+    virtual void  no_scroll(void);
+
     // virtual int   get_color(void);
     virtual void  clear();
     virtual void  move_cursor(int, int);
-    virtual void  no_scroll(void);
-    virtual void  scroll_up(); // only used privately
-    // virtual void  scroll_down();
     virtual void  output(char);
     virtual void  output(char *);
     virtual void  output_line(char *);
@@ -63,7 +111,6 @@ public:
     virtual void  repeat(char a, int len);
     virtual void  draw_border(void);
     virtual void  draw_border_horiz(void);
-    virtual void  reverse_mode(int);
     virtual int   get_size_x(void);
     virtual int   get_size_y(void);
     virtual void  set_char(int x, int y, char);
