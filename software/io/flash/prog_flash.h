e58363029454f3715c4cd2e3487d49da1053f3c6
GideonZ <gideon.zweijtzer@gmail.com>
2018-03-13 15:14:34 +0100

    U64 related changes.

--- software/io/flash/prog_flash.h
+++ software/io/flash/prog_flash.h
@@ -11,7 +11,8 @@
 #include "flash.h"
 #include "screen.h"
 
-bool flash_buffer(Flash *flash, Screen *screen, int id, void *buffer, void *buf_end, char *version, char *descr);
-bool flash_buffer_at(Flash *flash, Screen *screen, int address, bool header, void *buffer, void *buf_end, char *version, char *descr);
+bool flash_buffer(Flash *flash, Screen *screen, int id, void *buffer, const void *buf_end, const char *version, const char *descr);
+bool flash_buffer_at(Flash *flash, Screen *screen, int address, bool header, void *buffer, void *buf_end, const char *version, const char *descr);
+bool flash_buffer_length(Flash *flash, Screen *screen, int address, bool header, void *buffer, uint32_t length, const char *version, const char *descr);
 
 #endif /* PROG_FLASH_H_ */
