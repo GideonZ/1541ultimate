c2ee2bfb9cb4c53199cf4d1fa7d99ffa5759c22b
GideonZ <gideon.zweijtzer@gmail.com>
2018-07-09 19:40:39 +0200

    Added feature to sample SIDs and send sample over Ethernet (U64 only)

--- software/network/socket_dma.h
+++ software/network/socket_dma.h
@@ -16,7 +16,10 @@
 
 class SocketDMA {
 	static void dmaThread(void *a);
-	static void parseBuffer(void *load_buffer, int length);
+	static void performCommand(int socket, void *load_buffer, int length, uint16_t cmd, uint16_t len);
+	static int  readSocket(int socket, void *buffer, int max_remain);
+	static int  writeSocket(int socket, void *buffer, int length);
+
 	uint8_t load_buffer[SOCKET_BUFFER_SIZE];
 public:
 	SocketDMA();
