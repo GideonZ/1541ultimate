0b3186197edc1f6f73047d86e5bb3767f4b5f62f
Gideon Zweijtzer <gideon.zweijtzer@technolution.nl>
2018-07-13 18:43:59 +0200

    Copied ulpi_tx improvements and simplification from other project

--- fpga/io/usb2/vhdl_source/ulpi_tx.vhd
+++ fpga/io/usb2/vhdl_source/ulpi_tx.vhd
@@ -20,7 +20,6 @@ port (
     tx_valid    : out   std_logic;
     tx_next     : in    std_logic;
     tx_data     : out   std_logic_vector(7 downto 0);
-    rx_busy     : in    std_logic;
     
     -- CRC calculator
     crc_sync        : out std_logic;
@@ -40,7 +39,7 @@ end ulpi_tx;
 
 architecture gideon of ulpi_tx is
     type t_state is (idle, crc_1, crc_2, token0, token1, token2, token3,
-                     transmit, wait4next, write_end, handshake, gap, gap2);
+                     transmit, wait4next, handshake, gap, gap2, wait_for_se0, wait_for_j);
     signal state    : t_state;
 
     signal tx_data_i    : std_logic_vector(7 downto 0);
@@ -52,21 +51,11 @@ architecture gideon of ulpi_tx is
 
     signal rd_data      : std_logic_vector(7 downto 0);
     signal rd_last      : std_logic;
-    signal rd_next      : std_logic;
     signal token_vector : std_logic_vector(18 downto 0);
     signal long         : boolean;
-    signal fifo_flush   : std_logic;
     signal busy         : std_logic;
     signal sending_sof  : boolean;
-    signal tx_allowed   : std_logic;
-    signal start_value  : unsigned(10 downto 0);
-    signal start_timer  : std_logic;
-    
-    -- internal fifo is 3 bytes as it seems. 3 bytes is at max 40 bits incl. 1.5 SE0 EOP. at Full speed this is 40*5 = 200 clocks
-    -- at low speed this is 40*40 clocks = 1600 
-    type t_int_array is array (natural range <>) of integer;
-    constant c_gap_values : t_int_array(0 to 3) := ( 1599, 199, 13, 13 ); 
-
+        
     -- XILINX USB STICK:
     -- On high speed, gap values 0x05 - 0x25 WORK.. (bigger than 0x25 doesn't, smaller than 0x05 doesn't..)
     -- TRUST USB 2.0 Hub:
@@ -81,7 +70,7 @@ architecture gideon of ulpi_tx is
     attribute fsm_encoding of state : signal is "sequential";
 begin
     usb_tx_resp.request_ack <= (usb_tx_req.send_token or usb_tx_req.send_handsh or usb_tx_req.send_packet or usb_tx_req.send_split)
-                                  when (state = idle) and (tx_allowed = '1') else '0';
+                                  when (state = idle) else '0';
     
     usb_tx_resp.busy        <= busy;
 
@@ -93,40 +82,37 @@ begin
                 tx_start  <= '0';
                 tx_valid  <= '0';
                 tx_last_i <= '0';
-                fifo_flush <= '0';
                 tx_data_i <= X"00";
                 no_data_d <= usb_tx_req.no_data;
                 long <= false;
                 sending_sof <= usb_tx_req.pid = c_pid_sof;
 
-                if tx_allowed = '1' then
-                    if usb_tx_req.send_token='1' and g_support_token then
-                        token_vector <= token_to_vector(usb_tx_req.token) & X"00";
-                        tx_start <= '1';
-                        tx_valid <= '1';
-                        tx_data_i <= X"4" & usb_tx_req.pid;
-                        state <= token1;
-                    elsif usb_tx_req.send_split='1' and g_support_split then
-                        token_vector <= split_token_to_vector(usb_tx_req.split_token);
-                        tx_start <= '1';
-                        tx_valid <= '1';
-                        tx_data_i <= X"4" & usb_tx_req.pid;
-                        long <= true;
-                        state <= token0;
-                    elsif usb_tx_req.send_handsh='1' then
-                        tx_start <= '1';
-                        tx_valid <= '1';
-                        tx_data_i <= X"4" & usb_tx_req.pid;
-                        tx_last_i <= '1';
-                        state <= handshake;
-                    elsif usb_tx_req.send_packet='1' then
-                        tx_start <= '1';
-                        tx_valid <= '1';
-                        tx_data_i <= X"4" & usb_tx_req.pid;
-                        state <= wait4next;
-                    end if;             
-                end if;
-                            
+                if usb_tx_req.send_token='1' and g_support_token then
+                    token_vector <= token_to_vector(usb_tx_req.token) & X"00";
+                    tx_start <= '1';
+                    tx_valid <= '1';
+                    tx_data_i <= X"4" & usb_tx_req.pid;
+                    state <= token1;
+                elsif usb_tx_req.send_split='1' and g_support_split then
+                    token_vector <= split_token_to_vector(usb_tx_req.split_token);
+                    tx_start <= '1';
+                    tx_valid <= '1';
+                    tx_data_i <= X"4" & usb_tx_req.pid;
+                    long <= true;
+                    state <= token0;
+                elsif usb_tx_req.send_handsh='1' then
+                    tx_start <= '1';
+                    tx_valid <= '1';
+                    tx_data_i <= X"4" & usb_tx_req.pid;
+                    tx_last_i <= '1';
+                    state <= handshake;
+                elsif usb_tx_req.send_packet='1' then
+                    tx_start <= '1';
+                    tx_valid <= '1';
+                    tx_data_i <= X"4" & usb_tx_req.pid;
+                    state <= wait4next;
+                end if;             
+            
             when wait4next =>
                 if tx_next='1' then
                     tx_start <= '0';
@@ -138,26 +124,18 @@ begin
                     end if;
                 end if;
 
-            when handshake =>
+            when transmit =>
                 if tx_next='1' then
-                    tx_start  <= '0';
-                    tx_valid  <= '0';
-                    tx_last_i <= '0';
-                    state <= gap;
+                    tx_start <= '0';
+                end if;                
+                if tx_next='1' and rd_last='1' then
+                    state <= crc_1;
                 end if;
 
-            when write_end =>
-                if tx_next='1' then
-                    tx_start  <= '0';
-                    tx_valid  <= '0';
-                    tx_last_i <= '0';
-                    state <= idle;
-                end if;
-                
             when crc_1 =>
                 if tx_next = '1' then
+                    tx_start <= '0';
                     tx_last_i <= '1';
-                    fifo_flush <= '1';
                     state <= crc_2;
                 end if;
             
@@ -168,6 +146,14 @@ begin
                     state <= gap;
                 end if;
                 
+            when handshake =>
+                if tx_next='1' then
+                    tx_start  <= '0';
+                    tx_valid  <= '0';
+                    tx_last_i <= '0';
+                    state <= gap;
+                end if;
+
             when token0 =>
                 if tx_next = '1' then
                     tx_start  <= '0';
@@ -200,33 +186,62 @@ begin
                     state <= gap;
                 end if;
 
-            when gap => -- pulse timer
-                state <= gap2;
-            
-            when gap2 =>
-                if tx_allowed = '1' then
-                    state <= idle;
+            when gap =>
+                if g_simulation then
+                    gap_count <= 15;
+                    state <= gap2;
+                elsif speed(1) = '1' then -- high speed
+                    if sending_sof then
+                        gap_count <= 21; -- compensate for long EOP
+                    else
+                        gap_count <= 17;
+                    end if;
+                    state <= gap2;
+                else -- low / full speed 
+                    state <= wait_for_se0;
                 end if;
                 
-            when transmit =>
-                if tx_next='1' and rd_last='1' then
-                    state <= crc_1;
+            when gap2 => -- Optimization? look for squelch in high speed
+                if status(4)='1' then -- when receiving data, we don't need to wait any longer.
+                    state <= idle;
+                elsif gap_count = 0 then
+                    state <= idle;
+                else
+                    gap_count <= gap_count - 1;
                 end if;
 
+            when wait_for_se0 =>
+                if status(4)='1' then -- when receiving data, we don't need to wait any longer.
+                    state <= idle;
+                elsif status(1 downto 0) = "00" then
+                    state <= wait_for_j;
+                end if;
+            
+            when wait_for_j =>
+                if speed(0) = '0' then -- low speed
+                    gap_count <= 175;
+                else                   -- full speed                 
+                    gap_count <= 12;
+                end if;
+
+                if status(4)='1' then -- when receiving data, we don't need to wait any longer.
+                    state <= idle;
+                elsif status(1 downto 0) /= "00" then
+                    state <= gap2;
+                end if;
+                
             when others => 
                 null;
             end case;                                    
 
             if reset='1' then
                 state  <= idle;
-                fifo_flush <= '0';
             end if;
         end if;
     end process;
 
     crc_dvalid   <= '1' when (state = transmit) and tx_next='1' else '0';
-    --crc_sync     <= '1' when (state = idle) else '0';
-    crc_sync     <= usb_tx_req.send_packet;
+    crc_sync     <= '1' when (state = idle) else '0';
     busy         <= '0' when (state = idle) else '1'; -- or (state = gap) else '1';
         
     g_token: if g_support_token generate
@@ -246,6 +261,8 @@ begin
 
     end generate;
     
+    data_to_crc <= rd_data;
+
     with state select tx_data <=
         rd_data               when transmit,
         data_crc(7 downto 0)  when crc_1,
@@ -253,56 +270,13 @@ begin
         tx_data_i             when others;
 
     tx_last <= tx_last_i;
-    rd_next <= '1' when (tx_next = '1') and (state = transmit) else '0';
-    
-    i_tx_fifo: entity work.srl_fifo
-    generic map (
-        Width       => 9,
-        Threshold   => 13 )
-    port map (
-        clock               => clock,
-        reset               => reset,
-        GetElement          => rd_next,
-        PutElement          => usb_tx_req.data_valid,
-        FlushFifo           => fifo_flush,
-        DataIn(8)           => usb_tx_req.data_last,
-        DataIn(7 downto 0)  => usb_tx_req.data,
-        DataOut(8)          => rd_last,
-        DataOut(7 downto 0) => rd_data,
-        SpaceInFifo         => open,
-        AlmostFull          => usb_tx_resp.data_wait,
-        DataInFifo          => open );
-    
-    data_to_crc <= rd_data;
 
-    start_timer <= '1' when state = gap else rx_busy; -- we start the tx_backoff timer when we are receiving, or when we finished transmitting
+    rd_data <= usb_tx_req.data;
+    rd_last <= usb_tx_req.data_last;
 
-    process(sending_sof, speed, status)
-    begin
-        if g_simulation then
-            start_value <= to_unsigned(15, start_value'length);
-        elsif rx_busy = '1' then
-            start_value <= to_unsigned(12, start_value'length);
-        elsif sending_sof and speed(1)='1' then
-            start_value <= to_unsigned(22, start_value'length);
-        else
-            case speed is
-            when "00" => start_value <= to_unsigned(c_gap_values(0), start_value'length);
-            when "01" => start_value <= to_unsigned(c_gap_values(1), start_value'length);
-            when others => start_value <= to_unsigned(c_gap_values(2), start_value'length);
-            end case;
-        end if;
-    end process;
-    
-    i_timer: entity work.timer
-    generic map (
-        g_reset     => '1',
-        g_width     => 11 )
-    port map (
-        clock       => clock,
-        reset       => reset,
-        start       => start_timer,
-        start_value => start_value,
-        timeout     => tx_allowed );
+    with state select 
+        usb_tx_resp.data_wait <=
+            not tx_next when transmit,
+            '1' when others; 
     
 end gideon;
