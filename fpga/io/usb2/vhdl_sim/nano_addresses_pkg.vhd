5310d876bc67f048cfd1d2ffde2d6b59c90f260b
GideonZ <gideon.zweijtzer@gmail.com>
2018-07-14 22:49:51 +0200

    New nano stuff is starting to work a bit..

--- fpga/io/usb2/vhdl_sim/nano_addresses_pkg.vhd
+++ fpga/io/usb2/vhdl_sim/nano_addresses_pkg.vhd
@@ -11,14 +11,19 @@ use ieee.numeric_std.all;
 
 package nano_addresses_pkg is
 
-    constant Command          : unsigned(19 downto 0) := X"007E0";
-    constant Command_DevEP    : unsigned(19 downto 0) := X"007E2";
-    constant Command_Length   : unsigned(19 downto 0) := X"007E4";
-    constant Command_MaxTrans : unsigned(19 downto 0) := X"007E6";
-    constant Command_MemHi    : unsigned(19 downto 0) := X"007E8";
-    constant Command_MemLo    : unsigned(19 downto 0) := X"007EA";
-    constant Command_SplitCtl : unsigned(19 downto 0) := X"007EC";
-    constant Command_Result   : unsigned(19 downto 0) := X"007EE";
+    constant Command          : unsigned(19 downto 0) := X"00600";
+    constant Command_DevEP    : unsigned(19 downto 0) := X"00602";
+    constant Command_Length   : unsigned(19 downto 0) := X"00604";
+    constant Command_MaxTrans : unsigned(19 downto 0) := X"00606";
+    constant Command_Interval : unsigned(19 downto 0) := X"00608";
+    constant Command_LastFrame: unsigned(19 downto 0) := X"0060A";
+    constant Command_SplitCtl : unsigned(19 downto 0) := X"0060C";
+    constant Command_Result   : unsigned(19 downto 0) := X"0060E";
+    constant Command_MemLo    : unsigned(19 downto 0) := X"00610";
+    constant Command_MemHi    : unsigned(19 downto 0) := X"00612";
+    constant Command_Started  : unsigned(19 downto 0) := X"00614";
+    constant Command_Timeout  : unsigned(19 downto 0) := X"00616";
+    constant Command_Size     : natural := 24; -- bytes    
 
     constant c_nano_simulation : unsigned(19 downto 0) := X"007D6";
     constant c_nano_dosuspend  : unsigned(19 downto 0) := X"007D8";
