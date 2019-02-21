84fa35e837103dbf1a06440dc4289d3140d3eb40
GideonZ <gideon.zweijtzer@gmail.com>
2019-02-21 16:26:33 +0100

    Improved and cleaned up Microblaze port of FreeRTOS

--- software/FreeRTOS/Source/portable/microblaze/portasm.s
+++ software/FreeRTOS/Source/portable/microblaze/portasm.s
@@ -71,126 +71,111 @@
 	.extern vTaskSwitchContext
 	.extern uxCriticalNesting
 	.extern pulISRStack
+    .extern ulPortTempR31
 
 	.global __FreeRTOS_interrupt_handler
 	.global VPortYieldASM
 	.global vStartFirstTask
-	.global microblaze_enable_interrupts
-	.global microblaze_disable_interrupts
 
 .macro portSAVE_CONTEXT
-	/* Make room for the context on the stack. */
-	addik r1, r1, -132
-	/* Save r31 so it can then be used. */
-	swi r31, r1, 4
-	/* Copy the msr into r31 - this is stacked later. */
-	mfs r31, rmsr
-	/* Stack general registers. */
-	swi r30, r1, 12
-	swi r29, r1, 16
-	swi r28, r1, 20
-	swi r27, r1, 24
-	swi r26, r1, 28
-	swi r25, r1, 32
-	swi r24, r1, 36
-	swi r23, r1, 40
-	swi r22, r1, 44
-	swi r21, r1, 48
-	swi r20, r1, 52
-	swi r19, r1, 56
-	swi r18, r1, 60
-	swi r17, r1, 64
-	swi r16, r1, 68
-	swi r15, r1, 72
-	swi r13, r1, 80
-	swi r12, r1, 84
-	swi r11, r1, 88
-	swi r10, r1, 92
-	swi r9, r1, 96
-	swi r8, r1, 100
-	swi r7, r1, 104
-	swi r6, r1, 108
-	swi r5, r1, 112
-	swi r4, r1, 116
-	swi r3, r1, 120
-	swi r2, r1, 124
-	/* Stack the critical section nesting value. */
-	lwi r3, r0, uxCriticalNesting
-	swi r3, r1, 128
-	/* Save the top of stack value to the TCB. */
-	lwi r3, r0, pxCurrentTCB
-	sw	r1, r0, r3
-	
+    /* Store R31 to temporary location. This works, because interrupts are off */
+    swi r31, r0, ulPortTempR31
+
+    /* Load pointer to processor state block */
+    lwi r31, r0, pxCurrentTCB
+    lw  r31, r31, r0  /* redirect to allocated block */
+
+    /* Now store everything */
+    swi r30, r31, 120
+    swi r29, r31, 116
+    swi r28, r31, 112
+    swi r27, r31, 108
+    swi r26, r31, 104
+    swi r25, r31, 100
+    swi r24, r31, 96
+    swi r23, r31, 92
+    swi r22, r31, 88
+    swi r21, r31, 84
+    swi r20, r31, 80
+    swi r19, r31, 76
+    swi r18, r31, 72
+    swi r17, r31, 68
+    swi r16, r31, 64
+    swi r15, r31, 60
+/*    swi r14, r31, 56   (stored later!) */
+    swi r13, r31, 52
+    swi r12, r31, 48
+    swi r11, r31, 44
+    swi r10, r31, 40
+    swi  r9, r31, 36
+    swi  r8, r31, 32
+    swi  r7, r31, 28
+    swi  r6, r31, 24
+    swi  r5, r31, 20
+    swi  r4, r31, 16
+    swi  r3, r31, 12
+    swi  r2, r31, 8
+    swi  r1, r31, 4
+
+    /* The alpha and the omega */
+    mfs  r4, rmsr
+    swi  r4, r31, 0
+    lwi  r3, r0, ulPortTempR31
+    swi  r3, r31, 124
+    lwi  r2, r0, uxCriticalNesting
+    swi  r2, r31, 128
 	.endm
 
 .macro portRESTORE_CONTEXT
-	/* Load the top of stack value from the TCB. */
-	lwi r3, r0, pxCurrentTCB
-	lw	r1, r0, r3	
-	/* Restore the general registers. */
-	lwi r31, r1, 4		
-	lwi r30, r1, 12		
-	lwi r29, r1, 16	
-	lwi r28, r1, 20	
-	lwi r27, r1, 24	
-	lwi r26, r1, 28	
-	lwi r25, r1, 32	
-	lwi r24, r1, 36	
-	lwi r23, r1, 40	
-	lwi r22, r1, 44	
-	lwi r21, r1, 48	
-	lwi r20, r1, 52	
-	lwi r19, r1, 56	
-	lwi r18, r1, 60	
-	lwi r17, r1, 64	
-	lwi r16, r1, 68	
-	lwi r15, r1, 72	
-	lwi r14, r1, 76	
-	lwi r13, r1, 80	
-	lwi r12, r1, 84	
-	lwi r11, r1, 88	
-	lwi r10, r1, 92	
-	lwi r9, r1, 96	
-	lwi r8, r1, 100	
-	lwi r7, r1, 104
-	lwi r6, r1, 108
-	lwi r5, r1, 112
-	lwi r4, r1, 116
-	lwi r2, r1, 124
+    /* Load pointer to processor state block */
+    lwi r31, r0, pxCurrentTCB
+    lw  r31, r31, r0  /* redirect to allocated block */
 
-	/* Load the critical nesting value. */
-	lwi r3, r1, 128
-	swi r3, r0, uxCriticalNesting
+    /* Restore the critical nesting value. */
+    lwi r3, r31, 128
+    swi r3, r0, uxCriticalNesting
 
-	/* Obtain the MSR value from the stack. */
-	lwi r3, r1, 8
+    /* Now reload everything */
+    lwi r30, r31, 120
+    lwi r29, r31, 116
+    lwi r28, r31, 112
+    lwi r27, r31, 108
+    lwi r26, r31, 104
+    lwi r25, r31, 100
+    lwi r24, r31, 96
+    lwi r23, r31, 92
+    lwi r22, r31, 88
+    lwi r21, r31, 84
+    lwi r20, r31, 80
+    lwi r19, r31, 76
+    lwi r18, r31, 72
+    lwi r17, r31, 68
+    lwi r16, r31, 64
+    lwi r15, r31, 60
+    lwi r14, r31, 56
+    lwi r13, r31, 52
+    lwi r12, r31, 48
+    lwi r11, r31, 44
+    lwi r10, r31, 40
+    lwi  r9, r31, 36
+    lwi  r8, r31, 32
+    lwi  r7, r31, 28
+    lwi  r6, r31, 24
+    lwi  r5, r31, 20
+    lwi  r4, r31, 16
+/* We need to use R3 still */
+    lwi  r2, r31, 8
+    lwi  r1, r31, 4
 
-	/* Are interrupts enabled in the MSR?  If so return using an return from 
-	interrupt instruction to ensure interrupts are enabled only once the task
-	is running again. */
-	andi r3, r3, 2
-	beqid r3, 36
-	or r0, r0, r0
-
-	/* Reload the rmsr from the stack, clear the enable interrupt bit in the
-	value before saving back to rmsr register, then return enabling interrupts
-	as we return. */
-	lwi r3, r1, 8
-	andi r3, r3, ~2
-	mts rmsr, r3
-	lwi r3, r1, 120
-	addik r1, r1, 132
-	rtid r14, 0
-	or r0, r0, r0
-
-	/* Reload the rmsr from the stack, place it in the rmsr register, and
-	return without enabling interrupts. */
-	lwi r3, r1, 8
-	mts rmsr, r3
-	lwi r3, r1, 120
-	addik r1, r1, 132
-	rtsd r14, 0
-	or r0, r0, r0
+    /* Restore the MSR, except the I flag, RTID will set it. */
+    lwi r3, r31, 0    /* 8 */
+    andi r3, r3, ~2   /* 12 */
+    mts rmsr, r3      /* 16 */
+    /* Restore the remaining registers that we were using */
+    lwi  r3, r31, 12  /* 20 */
+    lwi  r31, r31, 124 /* 24 */
+    rtid r14, 0       /* 28 Last instruction has IMM!  */
+    nop               /* 32 */
 
 	.endm
 
@@ -200,46 +185,39 @@
 
 __FreeRTOS_interrupt_handler:
 	portSAVE_CONTEXT
-	/* Entered via an interrupt so interrupts must be enabled in msr. */
-	ori r31, r31, 2
-	/* Stack msr. */
-	swi r31, r1, 8
-	/* Stack the return address.  As we entered via an interrupt we do
-	not need to modify the return address prior to stacking. */
-	swi r14, r1, 76
+	/* Store the return address.  As we entered via an interrupt we do
+	not need to modify the return address prior to storing R14. */
+	swi r14, r31, 56
+
 	/* Now switch to use the ISR stack. */
-	lwi r3, r0, pulISRStack
-	add r1, r3, r0
+    lwi r1, r0, pulISRStack
+
 	bralid r15, vTaskISRHandler
 	or r0, r0, r0
+
 	portRESTORE_CONTEXT
 
 
+/* This is written in a way that the macro can just jump here, with link to R14 (bralid R14, VPortYieldASM..) */
 VPortYieldASM:
+    msrclr r0, 0x02 /* Turn interrupts off */
 	portSAVE_CONTEXT
-	/* Stack msr. */
-	swi r31, r1, 8
+
 	/* Modify the return address so we return to the instruction after the
 	exception. */
-	addi r14, r14, 8
-	swi r14, r1, 76
-	/* Now switch to use the ISR stack. */
-	lwi r3, r0, pulISRStack
-	add r1, r3, r0
+	addik r14, r14, 8
+	swi   r14, r31, 56
+
+	/* Now switch to use the ISR stack for the task switch. */
+    lwi r1, r0, pulISRStack
+
+    /* Call the scheduler */
 	bralid r15, vTaskSwitchContext
 	or r0, r0, r0
+
+    /* Restore and continue */
 	portRESTORE_CONTEXT
 
 vStartFirstTask:
 	portRESTORE_CONTEXT
 	
-;microblaze_enable_interrupts:
-;    msrset r0, 0x02
-;    rtsd r15, 8
-;    nop
-;
-;microblaze_disable_interrupts:
-;    msrclr r0, 0x02
-;    rtsd r15, 8
-;    nop
-
