3cd1b447d44e806140c66dda136ad30de12f2acd
GideonZ <gideon.zweijtzer@gmail.com>
2019-02-23 12:00:13 +0100

    Work done on simulation environment Microblaze

--- fpga/cpu_unit/vhdl_sim/mblite_simu_cached.vhd
+++ fpga/cpu_unit/vhdl_sim/mblite_simu_cached.vhd
@@ -22,8 +22,10 @@ architecture test of mblite_simu_cached is
     signal clock  : std_logic := '0';
     signal reset  : std_logic;
 
-    signal mmem_o : dmem_out_type;
-    signal mmem_i : dmem_in_type;
+    signal imem_o : dmem_out_type;
+    signal imem_i : dmem_in_type;
+    signal dmem_o : dmem_out_type;
+    signal dmem_i : dmem_in_type;
     signal irq_i  : std_logic := '0';
     signal irq_o  : std_logic;
 
@@ -32,35 +34,38 @@ architecture test of mblite_simu_cached is
     
     type t_mem_array is array(natural range <>) of std_logic_vector(31 downto 0);
     
-    shared variable memory  : t_mem_array(0 to 1048575) := (others => (others => '0')); -- 4MB
+    shared variable memory  : t_mem_array(0 to 1048575) := (others => X"AA55AA55"); -- 4MB
+    signal last_char : std_logic_vector(7 downto 0);
 
 BEGIN
     clock <= not clock after 10 ns;
     reset <= '1', '0' after 100 ns;
 
-    i_core: entity work.cached_mblite
+    i_core: entity mblite.cached_mblite
     port map (
         clock      => clock,
         reset      => reset,
         invalidate => invalidate,
         inv_addr   => inv_addr,
-        mmem_o     => mmem_o,
-        mmem_i     => mmem_i,
+        imem_o     => imem_o,
+        imem_i     => imem_i,
+        dmem_o     => dmem_o,
+        dmem_i     => dmem_i,
         irq_i      => irq_i,
         irq_o      => irq_o  );
     
     -- IRQ generation @ 100 kHz (every 10 us)
-    process
-    begin
-        for i in 1 to 50 loop
-            wait for 10 us;
-            wait until clock='1';
-            irq_i <= '1';
-            wait until clock='1';
-            irq_i <= '0';
-        end loop;
-        wait;        
-    end process;
+--    process
+--    begin
+--        for i in 1 to 50 loop
+--            wait for 10 us;
+--            wait until clock='1';
+--            irq_i <= '1';
+--            wait until clock='1';
+--            irq_i <= '0';
+--        end loop;
+--        wait;        
+--    end process;
 
     process
     begin
@@ -83,26 +88,32 @@ BEGIN
         variable byte : std_logic_vector(7 downto 0);
     begin
         if rising_edge(clock) then
-            mmem_i.dat_i <= (others => 'X');
-            if mmem_o.ena_o = '1' then
-                if mmem_o.adr_o(31 downto 25) = "0000000" then
-                    if mmem_o.we_o = '1' then
+            if imem_o.ena_o = '1' then
+                imem_i.dat_i <= memory(to_integer(unsigned(imem_o.adr_o(21 downto 2))));
+--                if (imem_i.dat_i(31 downto 26) = "000101") and (imem_i.dat_i(1 downto 0) = "01") then
+--                    report "Suspicious CMPS" severity warning;
+--                end if;
+            end if;
+            if dmem_o.ena_o = '1' then
+                if dmem_o.adr_o(31 downto 25) = "0000000" then
+                    if dmem_o.we_o = '1' then
                         for i in 0 to 3 loop
-                            if mmem_o.sel_o(i) = '1' then
-                                memory(to_integer(unsigned(mmem_o.adr_o(21 downto 2))))(i*8+7 downto i*8) := mmem_o.dat_o(i*8+7 downto i*8);
+                            if dmem_o.sel_o(i) = '1' then
+                                memory(to_integer(unsigned(dmem_o.adr_o(21 downto 2))))(i*8+7 downto i*8) := dmem_o.dat_o(i*8+7 downto i*8);
                             end if;
                         end loop;
                     else -- read
-                        mmem_i.dat_i <= memory(to_integer(unsigned(mmem_o.adr_o(21 downto 2))));
+                        dmem_i.dat_i <= memory(to_integer(unsigned(dmem_o.adr_o(21 downto 2))));
                     end if;
                 else -- I/O
-                    if mmem_o.we_o = '1' then -- write
-                        case mmem_o.adr_o(19 downto 0) is
+                    if dmem_o.we_o = '1' then -- write
+                        case dmem_o.adr_o(19 downto 0) is
                         when X"00000" => -- interrupt
                             null;
                         when X"00010" => -- UART_DATA
-                            byte := mmem_o.dat_o(31 downto 24);
+                            byte := dmem_o.dat_o(31 downto 24);
                             char := character'val(to_integer(unsigned(byte)));
+                            last_char <= byte;
                             if byte = X"0D" then
                                 -- Ignore character 13
                             elsif byte = X"0A" then
@@ -113,30 +124,31 @@ BEGIN
                                 write(s, char);
                             end if;
                         when others =>
-                            report "I/O write to " & hstr(mmem_o.adr_o) & " dropped";
+                            report "I/O write to " & hstr(dmem_o.adr_o) & " dropped";
                         end case;
                     else -- read
-                        case mmem_o.adr_o(19 downto 0) is
+                        case dmem_o.adr_o(19 downto 0) is
                         when X"0000C" => -- Capabilities
-                            mmem_i.dat_i <= X"00000002";
+                            dmem_i.dat_i <= X"00000002";
                         when X"00012" => -- UART_FLAGS
-                            mmem_i.dat_i <= X"40404040";
+                            dmem_i.dat_i <= X"40404040";
                         when X"2000A" => -- 1541_A memmap
-                            mmem_i.dat_i <= X"3F3F3F3F";
+                            dmem_i.dat_i <= X"3F3F3F3F";
                         when X"2000B" => -- 1541_A audiomap
-                            mmem_i.dat_i <= X"3E3E3E3E";
+                            dmem_i.dat_i <= X"3E3E3E3E";
                         when others =>
-                            report "I/O read to " & hstr(mmem_o.adr_o) & " dropped";
-                            mmem_i.dat_i <= X"00000000";
+                            report "I/O read to " & hstr(dmem_o.adr_o) & " dropped";
+                            dmem_i.dat_i <= X"00000000";
                         end case;
                     end if;
                 end if;
             end if;
 
             if reset = '1' then
-                mmem_i.ena_i <= '1';
+                imem_i.ena_i <= '1';
+                dmem_i.ena_i <= '1';
             end if;
         end if;
     end process;
-    
+   
 end architecture;
