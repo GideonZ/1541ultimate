36a51d6a7a66d5702118ad294c7dcdcc9e7f768a
WilfredC64 <wilfred.bos@xs4all.nl>
2019-04-06 10:28:57 +0200

    Updated version of player and copyright year

--- software/6502/sidcrt/sidcrt.asm
+++ software/6502/sidcrt/sidcrt.asm
@@ -1,10 +1,10 @@
 ;-----------------------------------------------------------------------
-; Ultimate SID Player V2.0a - Sidplayer for the Ultimate hardware
+; Ultimate SID Player V2.0b - Sidplayer for the Ultimate hardware
 ;
 ; Written by Wilfred Bos - April 2009
 ;                          August 2017 - January 2018
 ;
-; Copyright (c) 2009 - 2018 Wilfred Bos / Gideon Zweijtzer
+; Copyright (c) 2009 - 2019 Wilfred Bos / Gideon Zweijtzer
 ;
 ; Info:
 ;  Editing the source code should be done in a text editor which supports UTF-8.
@@ -90,7 +90,7 @@ ZERO_PAGE_ADDRESSES_MAIN = [
                 .byte >startNMI
                 .byte 'C' + $80, 'B' + $80, 'M' + $80, '8', '0'   ; CBM80
 
-                .text 0, 'Ultimate SID Player Cartridge V2.0a - Copyright (c) 2009-2018 Wilfred Bos / Gideon Zweijtzer', 0
+                .text 0, 'Ultimate SID Player Cartridge V2.0b - Copyright (c) 2009-2019 Wilfred Bos / Gideon Zweijtzer', 0
 
 startNMI        pla                 ; ignore pressing restore key
                 tay
