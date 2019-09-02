818c3a9da8745f93cc51ff69b7912bcb9ec551f6
GideonZ <gideon.zweijtzer@gmail.com>
2019-09-03 00:26:05 +0200

    Allow multiple configuration stores to be combined in one flash page for backward compatibility.

--- software/components/config.cc
+++ software/components/config.cc
@@ -26,7 +26,7 @@ extern "C" {
 #include <string.h>
 
 /*** CONFIGURATION MANAGER ***/
-ConfigManager :: ConfigManager() : stores(16, NULL)
+ConfigManager :: ConfigManager() : stores(16, NULL), pages(16, NULL)
 {
 	flash = get_flash();
 	if(!flash) {
@@ -48,26 +48,32 @@ ConfigManager :: ~ConfigManager()
 		delete s;
     }
     stores.clear_list();
+
+    ConfigPage *p;
+    for(int n = 0; n < stores.get_elements();n++) {
+        p = (ConfigPage *)pages[n];
+        delete p;
+    }
+
 }
 
-ConfigStore *ConfigManager :: register_store(uint32_t store_id, const char *name,
+ConfigStore *ConfigManager :: register_store(uint32_t page_id, const char *name,
                                 t_cfg_definition *defs, ConfigurableObject *ob) 
 {
 	if(!flash) {
-        printf("register_store %s: Can't open flash\n", name);
         flash = get_flash();
         printf("register_store %s: Can't open flash\n", name);
 		return NULL; // fail
 	}
-    if(store_id == 0) {
+    if(page_id == 0) {
         printf("ERROR: Requesting to register a store with ID=0\n");
         return NULL;
     }
 
-    // Check if the store was already opened
+    // Check if the store was already opened, just add the object to it
     for (int i=0; i<stores.get_elements(); i++) {
         ConfigStore *cfgStore = stores[i];
-        if (cfgStore->id == store_id) {
+        if (strcmp(cfgStore->get_store_name(), name) == 0) {
             if (ob) {
                 cfgStore->addObject(ob);
             }
@@ -76,36 +82,55 @@ ConfigStore *ConfigManager :: register_store(uint32_t store_id, const char *name
     }
 
     int page_size = flash->get_page_size();
-    uint32_t id;
-    ConfigStore *s;
 
-    for(int i=0;i<num_pages;i++) {
-        flash->read_config_page(i, 4, &id);
-        if (store_id == id) {
-            s = new ConfigStore(store_id, name, i, page_size, defs, ob); // TODO
-            s->read();
-            //printf("APPENDING STORE %p %s\n", s, s->get_name());
-            stores.append(s);
-            return s;
+    // Try to obtain page object that already represents the page in Flash
+    ConfigPage *page = NULL;
+    for(int n=0; n<pages.get_elements(); n++) {
+        ConfigPage *p = pages[n];
+        if (p->get_id() == page_id) {
+            page = p;
+            break;
         }
     }
-    //printf("Store %8x not found..\n", store_id);
-    for(int i=0;i<num_pages;i++) {
-        flash->read_config_page(i, 4, &id);
-        if (id == 0xFFFFFFFF) {
-            //printf("Found empty spot on config page %d, for ID=%8x. Size=%d. Defs=%p. Name=%s\n", i, store_id, page_size, defs, name);
-            s = new ConfigStore(store_id, name, i, page_size, defs, ob); // TODO
-            s->write();
-            stores.append(s);
-            return s;
+
+    bool write = false;
+    // Try to find the page in Flash
+    if (!page) {
+        uint32_t id;
+        for(int i=0;i<num_pages;i++) {
+            flash->read_config_page(i, 4, &id);
+            if (page_id == id) {
+                page = new ConfigPage(flash, id, i, page_size);
+                page->read();
+                pages.append(page);
+                break;
+            }
         }
     }
-    // no entry in flash found, no place to create one.
-    // create a temporary one in memory.
-    s = new ConfigStore(store_id, name, -1, page_size, defs, ob); // TODO
-    if(s) {
-        stores.append(s);
+
+    // if still no luck, try to create a page in flash
+    if (!page) {
+        uint32_t id;
+        for(int i=0;i<num_pages;i++) {
+            flash->read_config_page(i, 4, &id);
+            if (id == 0xFFFFFFFF) {
+                //printf("Found empty spot on config page %d, for ID=%8x. Size=%d. Defs=%p. Name=%s\n", i, store_id, page_size, defs, name);
+                page = new ConfigPage(flash, page_id, i, page_size);
+                pages.append(page);
+                write = true;
+                break;
+            }
+        }
     }
+
+    ConfigStore *s = new ConfigStore(page, name, defs, ob);
+    stores.append(s);
+    page->add_store(s);
+
+    if (write) {
+        page->write();
+    }
+    s->read();
     return s;
 }
 
@@ -119,36 +144,16 @@ void ConfigManager :: remove_store(ConfigStore *cfg)
     stores.remove(cfg);
 }
 
-ConfigStore *ConfigManager :: open_store(uint32_t id)
-{
-    ConfigStore *s;
-    for(int n = 0; n < stores.get_elements();n++) {
-        s = stores[n];
-        if(s->id == id)
-            return s;
-    }
-    return NULL;
-}
-
 //   ===================
 /*** CONFIGURATION STORE ***/
 //   ===================
-ConfigStore :: ConfigStore(uint32_t store_id, const char *name, int page, int page_size,
+ConfigStore :: ConfigStore(ConfigPage *page, const char *name,
                            t_cfg_definition *defs, ConfigurableObject *ob) : store_name(name), items(16, NULL), objects(4, NULL)
 {
-    //printf("Create configstore %8x with size %d..", store_id, page_size);
-    if(page_size)
-        mem_block = new uint8_t[page_size];
-    else
-        mem_block = NULL;
-        
-    block_size = page_size;
-    flash_page = page;
-    id = store_id;
     if (ob) {
         objects.append(ob);
     }
-
+    this->page = page;
     staleEffect = true;
     staleFlash = false;
     
@@ -170,8 +175,6 @@ ConfigStore :: ~ConfigStore()
 		delete i;
     }
     items.clear_list();
-    if(mem_block)
-    	delete mem_block;
 }
         
 void ConfigStore :: addObject(ConfigurableObject *obj)
@@ -185,14 +188,37 @@ int ConfigStore :: unregister(ConfigurableObject *obj)
     return objects.get_elements();
 }
 
-void ConfigStore :: pack()
+void ConfigPage :: pack(void)
 {
-    int remain = block_size;
-	memset(mem_block, 0xFF, block_size);
+    memset(mem_block, 0xFF, block_size);
+
     uint8_t *b = &mem_block[4];
+    (*(uint32_t *)mem_block) = id;
+    int remain = block_size - 4;
+
+    for(int n = 0; n < stores.get_elements(); n++) {
+        ConfigStore *s = stores[n];
+        remain = s->pack(b, remain);
+    }
+}
+
+void ConfigPage :: unpack(void)
+{
+    for(int n = 0; n < stores.get_elements(); n++) {
+        ConfigStore *s = stores[n];
+        s->unpack(mem_block, block_size);
+    }
+}
+
+void ConfigPage :: unpack(ConfigStore *s)
+{
+    s->unpack(mem_block, block_size);
+}
+
+int ConfigStore :: pack(uint8_t *b, int remain)
+{
     ConfigItem *i;
     int len;
-    (*(uint32_t *)mem_block) = id;
 
     //    printf("Packing ConfigStore %s.\n", store_name);
     for(int n = 0; n < items.get_elements();n++) {
@@ -206,6 +232,8 @@ void ConfigStore :: pack()
         }
     }
     *b = 0xFF;
+
+    return remain;
 }
 
 void ConfigStore :: effectuate()
@@ -222,20 +250,26 @@ void ConfigStore :: effectuate()
     
 void ConfigStore :: write()
 {
-	printf("Writing config store '%s' to flash, page %d..", store_name.c_str(), flash_page);
+	printf("Writing config store '%s' to flash..", store_name.c_str());
+	if(page) {
+	    page->write();
+        staleFlash = false;
+	}
+}
 
+void ConfigPage :: write()
+{
+    printf("Page: %d", flash_page);
 	pack();
-	Flash *flash = ConfigManager :: getConfigManager()->get_flash_access();
 	if(flash) {
 	    flash->write_config_page(flash_page, mem_block);
-	    staleFlash = false;
 	    printf(" done.\n");
 	} else {
 		printf(" error.\n");
 	}
 }
 
-void ConfigStore :: unpack()
+void ConfigStore :: unpack(uint8_t *mem_block, int block_size)
 {
     int index = 4;
     uint8_t id;
@@ -263,12 +297,18 @@ void ConfigStore :: unpack()
     }
 }
 
+void ConfigPage :: read()
+{
+    if(flash) {
+        flash->read_config_page(flash_page, block_size, mem_block);
+    }
+}
+
 void ConfigStore :: read()
 {
-	Flash *flash = ConfigManager :: getConfigManager()->get_flash_access();
-	if(flash) {
-	    flash->read_config_page(flash_page, block_size, mem_block);
-	    unpack();
+	if(page) {
+	    page->read();
+	    page->unpack(this);
 	}
 	staleEffect = true;
 	staleFlash = false;
