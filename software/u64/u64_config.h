d71f7329ae8a55b33b341160ee162b84583177e0
GideonZ <gideon.zweijtzer@gmail.com>
2019-09-13 22:14:06 +0200

    Of course, the mapping should be set correctly first.

--- software/u64/u64_config.h
+++ software/u64/u64_config.h
@@ -64,6 +64,7 @@ class U64Config : public ConfigurableObject, ObjectWithMenu, SubSystem
     void run_reset_task();
     static void show_mapping(uint8_t *base, uint8_t *mask, uint8_t *split, int count);
     static void DetectSidImpl(uint8_t *buffer) __attribute__ ((section ("detect_sid"), used));
+    static void S_SetupDetectionAddresses();
     static int S_SidDetector(int &sid1, int &sid2);
     static int detectRemakes(int socket);
     static int detectFPGASID(int socket);
