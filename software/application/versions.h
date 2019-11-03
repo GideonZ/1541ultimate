bc1e9c01c3d56eec5738760c30af72a267c87925
GideonZ <gideon.zweijtzer@gmail.com>
2019-11-03 11:43:29 +0100

    Better handling of stand alone mode, saves significant production time.

--- software/application/versions.h
+++ software/application/versions.h
@@ -22,7 +22,7 @@
 // alpha = \020
 // beta  = \021
 
-#define APPL_VERSION "3.6"
+#define APPL_VERSION "3.6a"
 #define BOOT_VERSION "V3.1"
 #define FPGA_VERSION "FPGA U2 V113"
 #define MINIMUM_FPGA_REV 0x13
