e068cfc0cde40480141107a4d94e503f69548304
GideonZ <gideon.zweijtzer@gmail.com>
2019-11-05 22:01:59 +0100

    Turned off ext I2C.. developer version doesn't build otherwise.

--- software/filetypes/filetype_bin.cc
+++ software/filetypes/filetype_bin.cc
@@ -297,6 +297,8 @@ int FileTypeBin :: execute(SubsysCommand *cmd)
             delete buffer;
             return 0;
         }
+#endif
+#if 0
         if (cmd->functionID == CMD_WRITE_EEPROM) {
             for(int i=0;i<256;i++) {
                 ext_i2c_write_byte(0xA0, i, buffer[i]);
