a79b338c70121b153cc3b9b1dadac99b36bf7028
GideonZ <gideon.zweijtzer@gmail.com>
2020-05-11 12:48:30 +0200

    Added support for non-aligned memory access in bus functional model.

--- fpga/ip/busses/vhdl_bfm/mem_bus_32_slave_bfm.vhd
+++ fpga/ip/busses/vhdl_bfm/mem_bus_32_slave_bfm.vhd
@@ -37,6 +37,9 @@ begin
 	resp.rack_tag <= req.tag when bound and req.request='1' else (others => '0');
 
     process(clock)
+        variable data : std_logic_vector(31 downto 0);
+        variable word_addr : unsigned(31 downto 2);
+        variable byte_addr : unsigned(1 downto 0);
     begin
         if rising_edge(clock) then
 			pipe(0 to g_latency-2) <= pipe(1 to g_latency-1);
@@ -48,9 +51,25 @@ begin
 				if pipe(0).request='1' then
 					if pipe(0).read_writen='1' then
 						resp.dack_tag <= pipe(0).tag;
-                        resp.data     <= read_memory_32(mem, "000000" & std_logic_vector(pipe(0).address));
+						data := read_memory_32(mem, "000000" & std_logic_vector(pipe(0).address));
+                        if pipe(0).address(1 downto 0) = "00" then
+                            resp.data     <= data;
+                        elsif pipe(0).address(1 downto 0) = "01" then
+                            resp.data     <= data(7 downto 0) & data(31 downto 8);
+                        elsif pipe(0).address(1 downto 0) = "10" then
+                            resp.data     <= data(15 downto 0) & data(31 downto 16);
+                        else
+                            resp.data     <= data(23 downto 0) & data(31 downto 24);
+                        end if;
                     else
-                        write_memory_32(mem, "000000" & std_logic_vector(pipe(0).address), pipe(0).data);
+                        word_addr := "000000" & pipe(0).address(25 downto 2);
+                        byte_addr := pipe(0).address(1 downto 0);
+                        for i in 0 to 3 loop
+                            if pipe(0).byte_en(i) = '1' then
+                                write_memory_8(mem, std_logic_vector(word_addr) & std_logic_vector(byte_addr), pipe(0).data(7+8*i downto 8*i));
+                            end if;
+                            byte_addr := byte_addr + 1;
+                        end loop;
 					end if;
 				end if;
 			end if; 
