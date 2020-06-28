d0cf0ebeab7a2c759f56d0b7de2617d676e57531
GideonZ <gideon.zweijtzer@gmail.com>
2020-06-28 20:05:59 +0200

    Increment FPGA version number. Just by one?

--- fpga/fpga_top/ultimate_fpga/vhdl_source/u2p_nios2_solo.vhd
+++ fpga/fpga_top/ultimate_fpga/vhdl_source/u2p_nios2_solo.vhd
@@ -471,7 +471,7 @@ begin
 
     i_logic: entity work.ultimate_logic_32
     generic map (
-        g_version       => X"16",
+        g_version       => X"17",
         g_simulation    => false,
         g_ultimate2plus => true,
         g_clock_freq    => 62_500_000,
