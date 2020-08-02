19dedaff3826c00f01c18632fde324dafafd1b36
GideonZ <gideon.zweijtzer@gmail.com>
2020-08-02 14:24:54 +0200

    Adds basic string functions for better readability in parser code.

--- software/components/pattern.h
+++ software/components/pattern.h
@@ -2,5 +2,7 @@
 #define PATTERN_H
 
 bool pattern_match(const char *p, const char *f, bool case_sensitive = false);
+void split_string(char sep, char *s, char **parts, int maxParts);
+bool isEmptyString(const char *c);
 
 #endif
