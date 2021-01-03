a722b162d70ecfb78fef4ed534bedcc564acb789
GideonZ <gideon.zweijtzer@gmail.com>
2021-01-03 12:06:12 +0100

    Updated ESP32 Scan application to IDF 4.2

--- software/wifi/scan/main/scan.c
+++ software/wifi/scan/main/scan.c
@@ -21,173 +21,140 @@
     can be sorted based on Authentication Mode or Signal Strength. The priority
     for the Authentication mode is:  WPA2 > WPA > WEP > Open
 */
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
-#include "freertos/semphr.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_system.h"
 
-/*Set the SSID and Password via "make menuconfig"*/
-#define DEFAULT_SSID CONFIG_WIFI_SSID
-#define DEFAULT_PWD CONFIG_WIFI_PASSWORD
+#define GPIO_LED0 4
+#define GPIO_LED1 16
+#define GPIO_LED2 19
 
-#if CONFIG_WIFI_ALL_CHANNEL_SCAN
-#define DEFAULT_SCAN_METHOD WIFI_ALL_CHANNEL_SCAN
-#elif CONFIG_WIFI_FAST_SCAN
-#define DEFAULT_SCAN_METHOD WIFI_FAST_SCAN
-#else
-#define DEFAULT_SCAN_METHOD WIFI_FAST_SCAN
-#endif /*CONFIG_SCAN_METHOD*/
-
-#if CONFIG_WIFI_CONNECT_AP_BY_SIGNAL
-#define DEFAULT_SORT_METHOD WIFI_CONNECT_AP_BY_SIGNAL
-#elif CONFIG_WIFI_CONNECT_AP_BY_SECURITY
-#define DEFAULT_SORT_METHOD WIFI_CONNECT_AP_BY_SECURITY
-#else
-#define DEFAULT_SORT_METHOD WIFI_CONNECT_AP_BY_SIGNAL
-#endif /*CONFIG_SORT_METHOD*/
-
-#if CONFIG_FAST_SCAN_THRESHOLD
-#define DEFAULT_RSSI CONFIG_FAST_SCAN_MINIMUM_SIGNAL
-#if CONFIG_EXAMPLE_OPEN
-#define DEFAULT_AUTHMODE WIFI_AUTH_OPEN
-#elif CONFIG_EXAMPLE_WEP
-#define DEFAULT_AUTHMODE WIFI_AUTH_WEP
-#elif CONFIG_EXAMPLE_WPA
-#define DEFAULT_AUTHMODE WIFI_AUTH_WPA_PSK
-#elif CONFIG_EXAMPLE_WPA2
-#define DEFAULT_AUTHMODE WIFI_AUTH_WPA2_PSK
-#else
-#define DEFAULT_AUTHMODE WIFI_AUTH_OPEN
-#endif
-#else
-#define DEFAULT_RSSI -127
-#define DEFAULT_AUTHMODE WIFI_AUTH_OPEN
-#endif /*CONFIG_FAST_SCAN_THRESHOLD*/
+#define DEFAULT_SCAN_LIST_SIZE 24
 
 static const char *TAG = "scan";
-static SemaphoreHandle_t mySemaphore;
 
-static esp_err_t event_handler(void *ctx, system_event_t *event)
+#define sendchar(x) {   putchar(x); }
+
+void send_slip_data(const uint8_t *buffer, int length)
 {
-    switch (event->event_id) {
-        case SYSTEM_EVENT_STA_START:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-            //ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        case SYSTEM_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-            ESP_LOGI(TAG, "Got IP: %s\n",
-                     ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-            break;
-        case SYSTEM_EVENT_STA_DISCONNECTED:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-            ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        case SYSTEM_EVENT_SCAN_DONE:
-            if (mySemaphore) {
-                xSemaphoreGive(mySemaphore);
-            }
-            break;
-        default:
-            ESP_LOGI(TAG, "Other event: %d", event->event_id);
-            break;
-    }
-    return ESP_OK;
-}
-
-static void my_scan(void)
-{
-    mySemaphore = xSemaphoreCreateBinary();
-    uint16_t number = 24;
-    static wifi_ap_record_t ap_records[24];
-
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    wifi_scan_config_t scan_config = {0};
-    scan_config.show_hidden = true;
-    //scan_config.scan_time.passive = 1000;
-    scan_config.scan_time.active.min = 150;
-    scan_config.scan_time.active.max = 500;
-    scan_config.scan_type = WIFI_SCAN_TYPE_ACTIVE;
-
-    esp_wifi_stop();
-    ESP_ERROR_CHECK( esp_wifi_set_mode (WIFI_MODE_STA) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-
-    uint32_t total = 0;
-    uint32_t scans = 0;
-    while(1) {
-        ESP_LOGI(TAG, "Start Scan");
-        ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config, false));
-        if (xSemaphoreTake(mySemaphore, 2000)) {
-            number = 24;
-            ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_records));
-            total += number;
-            scans ++;
-            printf("Scan complete. %d APs found. (Average: %u/100 APs. %u scans)\n", number, 100 * total / scans, scans);
-            for (int i=0; i<number; i++) {
-                printf("%-32s Channel: %2d Signal: %4d Mode: %d\n", ap_records[i].ssid, ap_records[i].primary, ap_records[i].rssi, ap_records[i].authmode);
-            }
+    // Escaped Body
+    for (int i = 0; i < length; i++) {
+        uint8_t data = *(buffer++);
+        if (data == 0xC0) {
+            sendchar(0xDB);
+            sendchar(0xDC);
+        } else if (data == 0xDB) {
+            sendchar(0xDB);
+            sendchar(0xDD);
         } else {
-            ESP_LOGI(TAG, "Timeout...");
+            sendchar(data);
         }
     }
-    //esp_wifi_stop();
 }
 
-/*
- Initialize Wi-Fi as sta and set scan method
+void send_slip_packet(const void *buffer, int length)
+{
+    // Start Character
+    sendchar(0xC0);
+    // Body
+    send_slip_data((uint8_t*)buffer, length);
+    // End Character
+    sendchar(0xC0);
+}
+
+/* 42 byte AP record */
+
+typedef struct {
+    uint8_t bssid[6];            // MAC address of AP
+    uint8_t ssid[33];            // SSID of AP
+    uint8_t channel;             // channel of AP
+    int8_t  rssi;                // signal strength of AP
+    uint8_t authmode;            // authmode of AP
+} ultimate_ap_record_t;
+
+typedef struct {
+    uint8_t packet_type;
+    uint8_t num_records;
+    ultimate_ap_record_t aps[DEFAULT_SCAN_LIST_SIZE];
+} ultimate_ap_records_t;
+
+ultimate_ap_records_t ult_records;
+
+// do not put it on the stack
+wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+
+// Initialize Wi-Fi as sta and set scan method
 static void wifi_scan(void)
 {
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "blah",
-            .password = "blah",
-            .scan_method = WIFI_ALL_CHANNEL_SCAN,
-            .sort_method = DEFAULT_SORT_METHOD,
-            .threshold.rssi = DEFAULT_RSSI,
-            .threshold.authmode = WIFI_AUTH_OPEN,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
 
-    wifi_scan_config_t scan = {
-        .ssid = NULL,  *< SSID of AP
-        .bssid = NULL, *< MAC address of AP
-        .channel = 0,  *< channel, scan the specific channel
-        .show_hidden = true,    *< enable to scan AP whose SSID is hidden
-        .scan_type = WIFI_SCAN_TYPE_PASSIVE,  *< scan type, active or passive
-        .scan_time = { .passive = 1000 }, //.active = { .min = 150, .max = 250 }},  //  scan time per channel
-    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_scan_start(NULL, true));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
+    if (ap_count > DEFAULT_SCAN_LIST_SIZE) {
+    	ap_count = DEFAULT_SCAN_LIST_SIZE;
+    }
+    for (int i = 0; i < ap_count; i++) {
+		printf("%-32s Channel: %2d Signal: %4d Mode: %d\n", ap_info[i].ssid, ap_info[i].primary, ap_info[i].rssi, ap_info[i].authmode);
+
+
+        memcpy(ult_records.aps[i].bssid, ap_info[i].bssid, 6);
+        memcpy(ult_records.aps[i].ssid, ap_info[i].ssid, 33);
+        ult_records.aps[i].channel = ap_info[i].primary;
+        ult_records.aps[i].rssi = ap_info[i].rssi;
+        ult_records.aps[i].authmode = ap_info[i].authmode;
+    }
+
+    ult_records.packet_type = 0x01;
+    ult_records.num_records = (uint8_t)ap_count;
+
+    send_spi_packet(&ult_records, sizeof(ultimate_ap_records_t));
 }
-*/
 
 void app_main()
 {
-    // Initialize NVS
+	ESP_LOGI(TAG, "App is Alive!");
+
+	// Initialize NVS
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
+	ESP_LOGI(TAG, "NVS Initialization done.");
 
-    my_scan();
+	spi_init();
+	ESP_LOGI(TAG, "SPI Initialization done.");
+
+    gpio_pad_select_gpio(GPIO_LED0);
+    gpio_pad_select_gpio(GPIO_LED1);
+    gpio_pad_select_gpio(GPIO_LED2);
+    gpio_set_direction(GPIO_LED0, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_LED1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_LED2, GPIO_MODE_OUTPUT);
+
+    wifi_scan();
 
     printf("That's all folks!\n");
     // Wait forever
