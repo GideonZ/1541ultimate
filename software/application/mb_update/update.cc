229afacb2955f0c710429ae13e59e31c4b3a9bfe
GideonZ <gideon.zweijtzer@gmail.com>
2021-07-08 19:50:35 +0200

    Changes to make U2 build again. NEEDS TESTING!

--- software/application/mb_update/update.cc
+++ software/application/mb_update/update.cc
@@ -12,6 +12,7 @@ extern "C" {
 
 #include "host.h"
 #include "c64.h"
+#include "c64_crt.h"
 #include "overlay.h"
 #include "flash.h"
 #include "screen.h"
@@ -33,32 +34,6 @@ extern uint8_t _mb_boot_700_b_start;
 extern uint8_t _mb_boot_700_b_end;
 extern uint8_t _mb_boot2_bin_start;
 extern uint8_t _mb_boot2_bin_end;
-
-extern uint8_t _ar5ntsc_bin_start;
-extern uint8_t _ar5pal_bin_start;
-extern uint8_t _ar6pal_bin_start;
-extern uint8_t _epyx_bin_start;
-extern uint8_t _final3_bin_start;
-extern uint8_t _rr38ntsc_bin_start;
-extern uint8_t _rr38pal_bin_start;
-extern uint8_t _ss5ntsc_bin_start;
-extern uint8_t _ss5pal_bin_start;
-extern uint8_t _tar_ntsc_bin_start;
-extern uint8_t _tar_pal_bin_start;
-extern uint8_t _kcs_bin_start;
-
-extern uint8_t _ar5ntsc_bin_end;
-extern uint8_t _ar5pal_bin_end;
-extern uint8_t _ar6pal_bin_end;
-extern uint8_t _epyx_bin_end;
-extern uint8_t _final3_bin_end;
-extern uint8_t _rr38ntsc_bin_end;
-extern uint8_t _rr38pal_bin_end;
-extern uint8_t _ss5ntsc_bin_end;
-extern uint8_t _ss5pal_bin_end;
-extern uint8_t _tar_ntsc_bin_end;
-extern uint8_t _tar_pal_bin_end;
-extern uint8_t _kcs_bin_end;
 
 extern uint8_t _1541_bin_start;
 extern uint8_t _snds1541_bin_start;
@@ -123,18 +98,16 @@ bool my_memcmp(void *a, void *b, int len)
 static void create_dir(const char *name)
 {
     FileManager *fm = FileManager :: getFileManager();
-    mstring dir("/flash/");
-    dir += name;
-    FRESULT fres = fm->create_dir(dir.c_str());
+    FRESULT fres = fm->create_dir(name);
     console_print(screen, "Creating dir '%s': %s\n", name, FileSystem :: get_error_string(fres));
 }
 
-static void write_flash_file(const char *name, uint8_t *data, int length)
+static void write_flash_file(const char *path, const char *name, uint8_t *data, int length)
 {
     File *f;
     uint32_t dummy;
     FileManager *fm = FileManager :: getFileManager();
-    FRESULT fres = fm->fopen("/flash", name, FA_CREATE_ALWAYS | FA_WRITE, &f);
+    FRESULT fres = fm->fopen(path, name, FA_CREATE_ALWAYS | FA_WRITE, &f);
     if (fres == FR_OK) {
         fres = f->write(data, length, &dummy);
         console_print(screen, "Writing %s: %s\n", name, FileSystem :: get_error_string(fres));
@@ -230,25 +203,12 @@ bool flash_buffer(int id, void *buffer, void *buf_end, const char *version, cons
     return true;    
 }
 
-void copy_rom(uint8_t *roms, int id, int *min, int *max, uint8_t *source, uint8_t *source_end)
+bool program_flash(bool do_update1, bool do_update2)
 {
-	t_flash_address image_address;
-	flash->get_image_addresses(id, &image_address);
-    if(image_address.start < *min)
-        *min = image_address.start;
-    if((image_address.start + image_address.max_length) > *max)
-        *max = image_address.start + image_address.max_length;
-    int length = (int)source_end - (int)source;
-    memcpy(roms+image_address.start, source, length);
-}
-
-bool program_flash(bool do_update1, bool do_update2, bool do_roms)
-{
-	uint8_t *roms;
-	
-    create_dir("roms");
-    write_flash_file("roms/1541.rom", &_1541_bin_start, 0x4000);
-    write_flash_file("roms/snds1541.bin", &_snds1541_bin_start, 0xC000);
+    create_dir(ROMS_DIRECTORY);
+    create_dir(CARTS_DIRECTORY);
+    write_flash_file(ROMS_DIRECTORY, "1541.rom", &_1541_bin_start, 0x4000);
+    write_flash_file(ROMS_DIRECTORY, "snds1541.bin", &_snds1541_bin_start, 0xC000);
 
 	flash->protect_disable();
 	last_sector = -1;
@@ -279,42 +239,6 @@ bool program_flash(bool do_update1, bool do_update2, bool do_roms)
                 user_interface->popup("Critical update failed. Retry?", BUTTON_OK);
         } while(!ok);
 	}
-    if(do_roms) {
-        if(flash->get_page_size() == 528) { // atmel 45.. we should program the roms in one block!
-            roms = (uint8_t *)malloc(0x210000);
-            memset(roms, 0xFF, 0x210000);
-            int min = 0x210000;
-            int max = 0x000000;
-            copy_rom(roms, FLASH_ID_AR5PAL,    &min, &max, &_ar5pal_bin_start,   &_ar5pal_bin_end   );
-            copy_rom(roms, FLASH_ID_AR6PAL,    &min, &max, &_ar6pal_bin_start,   &_ar6pal_bin_end   );
-            copy_rom(roms, FLASH_ID_FINAL3,    &min, &max, &_final3_bin_start,   &_final3_bin_end   );
-            copy_rom(roms, FLASH_ID_EPYX,      &min, &max, &_epyx_bin_start,     &_epyx_bin_end     );
-            copy_rom(roms, FLASH_ID_RR38PAL,   &min, &max, &_rr38pal_bin_start,  &_rr38pal_bin_end  );
-            copy_rom(roms, FLASH_ID_SS5PAL,    &min, &max, &_ss5pal_bin_start,   &_ss5pal_bin_end   );
-            copy_rom(roms, FLASH_ID_AR5NTSC,   &min, &max, &_ar5ntsc_bin_start,  &_ar5ntsc_bin_end  );
-            copy_rom(roms, FLASH_ID_RR38NTSC,  &min, &max, &_rr38ntsc_bin_start, &_rr38ntsc_bin_end );
-            copy_rom(roms, FLASH_ID_SS5NTSC,   &min, &max, &_ss5ntsc_bin_start,  &_ss5ntsc_bin_end  );
-            copy_rom(roms, FLASH_ID_TAR_PAL,   &min, &max, &_tar_pal_bin_start,  &_tar_pal_bin_end  );
-            copy_rom(roms, FLASH_ID_TAR_NTSC,  &min, &max, &_tar_ntsc_bin_start, &_tar_ntsc_bin_end );
-            copy_rom(roms, FLASH_ID_KCS,       &min, &max, &_kcs_bin_start,      &_kcs_bin_end      );
-            // console_print(screen, "All roms located from %p to %p.\n", min, max);
-            flash_buffer(FLASH_ID_ALL_ROMS, roms + min, roms + max, "", "all roms");
-            free(roms);
-        } else {
-    	    flash_buffer(FLASH_ID_AR5PAL,    &_ar5pal_bin_start,   &_ar5pal_bin_end,   "", "ar5pal");
-    	    flash_buffer(FLASH_ID_AR6PAL,    &_ar6pal_bin_start,   &_ar6pal_bin_end,   "", "ar6pal");
-    	    flash_buffer(FLASH_ID_FINAL3,    &_final3_bin_start,   &_final3_bin_end,   "", "final3");
-    	    flash_buffer(FLASH_ID_EPYX,      &_epyx_bin_start,     &_epyx_bin_end,     "", "epyx");
-    	    flash_buffer(FLASH_ID_RR38PAL,   &_rr38pal_bin_start,  &_rr38pal_bin_end,  "", "rr38pal");
-    	    flash_buffer(FLASH_ID_SS5PAL,    &_ss5pal_bin_start,   &_ss5pal_bin_end,   "", "ss5pal");
-    	    flash_buffer(FLASH_ID_AR5NTSC,   &_ar5ntsc_bin_start,  &_ar5ntsc_bin_end,  "", "ar5ntsc");
-    	    flash_buffer(FLASH_ID_RR38NTSC,  &_rr38ntsc_bin_start, &_rr38ntsc_bin_end, "", "rr38ntsc");
-    	    flash_buffer(FLASH_ID_SS5NTSC,   &_ss5ntsc_bin_start,  &_ss5ntsc_bin_end,  "", "ss5ntsc");
-    	    flash_buffer(FLASH_ID_TAR_PAL,   &_tar_pal_bin_start,  &_tar_pal_bin_end,  "", "tar_pal");
-    	    flash_buffer(FLASH_ID_TAR_NTSC,  &_tar_ntsc_bin_start, &_tar_ntsc_bin_end, "", "tar_ntsc");
-    	    flash_buffer(FLASH_ID_KCS,       &_kcs_bin_start,      &_kcs_bin_end,      "", "kcs");
-        }
-    }
 	if(do_update2) {
     	flash_buffer(FLASH_ID_APPL, &_ultimate_bin_start, &_ultimate_bin_end, APPL_VERSION, "Ultimate application");
     }
@@ -397,27 +321,21 @@ extern "C" void ultimate_main(void *)
 //    console_print(screen, "Flash protection disabled.\n");
 //    while(1);
 //
-    // virginity check
-	t_flash_address image_address;
-	flash->get_image_addresses(FLASH_ID_AR5PAL, &image_address);
-    flash->read_linear_addr(image_address.start, 2, byte_buffer);
-    if(byte_buffer[0] == 0xFF)
-        virgin = true;
-        
+
     do_update1 = need_update(FLASH_ID_BOOTFPGA, FPGA_VERSION, "FPGA") ||
                  need_update(FLASH_ID_BOOTAPP, BOOT_VERSION, "Secondary bootloader");
     do_update2 = need_update(FLASH_ID_APPL, APPL_VERSION, "Ultimate application");
 
     if(virgin) {
-        program_flash(true, true, true);
+        program_flash(true, true);
     } else if(do_update1 || do_update2) {
         if(user_interface->popup("Update required. Continue?", BUTTON_YES | BUTTON_NO) == BUTTON_YES) {
-            program_flash(true, true, true);
+            program_flash(true, true);
         }
     } else {
         int response = user_interface->popup("Update NOT required. Force?", BUTTON_YES | BUTTON_NO);
         if(response == BUTTON_YES) {
-            program_flash(true, true, true);
+            program_flash(true, true);
         }
 	}
 
@@ -434,15 +352,8 @@ extern "C" void ultimate_main(void *)
         }
     }
 
-//    console_print(screen, "\nTo avoid loading this updater again,\ndelete it from your media.\n");
-	
-//    wait_ms(2000);
-    
-//    user_interface->popup("Remove SDCard. Reboot?", BUTTON_OK);
     flash->reboot(0);
     
-//	console_print(screen, "\nPlease remove the SD card, and switch\noff your machine...\n");
-
     while(1)
         ;
 }
