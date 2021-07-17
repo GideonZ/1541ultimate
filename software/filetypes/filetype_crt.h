54c80a2502489efbcbd0a8821bf27cd44a442fb8
GideonZ <gideon.zweijtzer@gmail.com>
2021-07-17 16:40:49 +0200

    Temporary command to load CRTs from TCP.

--- software/filetypes/filetype_crt.h
+++ software/filetypes/filetype_crt.h
@@ -8,16 +8,15 @@ class FileTypeCRT : public FileType
 {
 	BrowsableDirEntry *node;
 
-    static int execute_st(SubsysCommand *cmd);
-    int execute(SubsysCommand *cmd);
     static int executeFlash_st(SubsysCommand *cmd);
-    int executeFlash(SubsysCommand *cmd);
 public:
     FileTypeCRT(BrowsableDirEntry *node);
     ~FileTypeCRT();
 
     int   fetch_context_items(IndexedList<Action *> &list);
     static FileType *test_type(BrowsableDirEntry *obj);
+
+    static int execute_st(SubsysCommand *cmd);
 };
 
 
