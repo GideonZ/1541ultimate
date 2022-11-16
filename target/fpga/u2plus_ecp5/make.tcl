d6b28ecf4be5f4864e56955a89ec5bb5612ff05b
GideonZ <gideon.zweijtzer@gmail.com>
2022-11-16 10:39:41 +0100

    Added make build scripts for Lattice.

---
+++ target/fpga/u2plus_ecp5/make.tcl
@@ -0,0 +1,9 @@
+prj_project open "u2p_ecp5.ldf"
+prj_run Synthesis -impl impl1
+prj_run Translate -impl impl1
+prj_run Map -impl impl1
+prj_run PAR -impl impl1
+prj_run PAR -impl impl1 -task PARTrace
+prj_run Export -impl impl1 -task Bitgen
+prj_project close
+
