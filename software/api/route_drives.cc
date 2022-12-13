a45d23c5654c2caab226e4be61f5c44e3cf87d3c
GideonZ <gideon.zweijtzer@gmail.com>
2022-12-13 23:08:37 +0100

    Improves drives route.

--- software/api/route_drives.cc
+++ software/api/route_drives.cc
@@ -15,20 +15,20 @@ ImageTypeToInt imageTypeToInt;
 ImageTypeToCommand imageTypeToCommand;
 ModeToInt modeToInt;
 
-static void drive_info(JSON_Object *obj, C1541 *drive, const char *letter)
+static void drive_info(JSON_List *obj, C1541 *drive, const char *letter)
 {
     mstring path, name;
     drive->get_last_mounted_file(path, name);
-    obj->add(letter, JSON::Obj()
+    obj->add(JSON::Obj()->add(letter, JSON::Obj()
         ->add("enabled", drive->get_drive_power())
         ->add("bus_id", drive->get_current_iec_address())
         ->add("type", drive->get_drive_type_string())
         ->add("rom", drive->get_drive_rom_file())
         ->add("image_file", name.c_str())
-        ->add("image_path", path.c_str()));
+        ->add("image_path", path.c_str())));
 }
 
-static void iec_info(JSON_Object *obj)
+static void iec_info(JSON_List *obj)
 {
     char buffer[64];
     iec_if.get_error_string(buffer);
@@ -41,21 +41,23 @@ static void iec_info(JSON_Object *obj)
         }
     }
 
-    obj->add("softiec", JSON::Obj()
+    obj->add(JSON::Obj()->add("softiec", JSON::Obj()
         ->add("enabled", iec_if.iec_enable))
         ->add("bus_id", iec_if.get_current_iec_address())
         ->add("type", "DOS emulation")
         ->add("last_error", buffer)
-        ->add("partitions", partitions);
+        ->add("partitions", partitions));
 }
 
 // List all the available drives
 API_CALL(GET, drives, none, NULL, ARRAY({P_END}))
 {
-    JSON_Object *obj = JSON::Obj();
-    if (c1541_A) drive_info(obj, c1541_A, "A");
-    if (c1541_B) drive_info(obj, c1541_B, "A");
-    iec_info(obj);
+    JSON_List *errors = JSON::List();
+    JSON_List *drives = JSON::List();
+    JSON_Object *obj = JSON::Obj()->add("drives", drives)->add("errors", errors);
+    if (c1541_A) drive_info(drives, c1541_A, "a");
+    if (c1541_B) drive_info(drives, c1541_B, "b");
+    // iec_info(obj);
     build_response(resp, 200, obj->render());
     delete obj;
 }
@@ -88,7 +90,7 @@ API_CALL(PUT, drives, mount, NULL, ARRAY({{ "image", P_REQUIRED }, { "type", P_O
     printf("Mount disk from path '%s' on drive '%s'\n", args["image"], args.get_path());
     char ext[4];
     get_extension(args["image"], ext);
-    api_mount(resp, args.get_path(), args["image"], args.get_or("type", ext), args["mode"]);
+    api_mount(resp, args["image"], args.get_path(), args.get_or("type", ext), args["mode"]);
 }
 
 API_CALL(POST, drives, mount, &attachment_writer, ARRAY({ { "type", P_OPTIONAL }, { "mode", P_OPTIONAL }, P_END }))
@@ -105,5 +107,5 @@ API_CALL(POST, drives, mount, &attachment_writer, ARRAY({ { "type", P_OPTIONAL }
     printf("Mount disk from upload: '%s'\n", fn);
     char ext[4];
     get_extension(fn, ext);
-    api_mount(resp, args.get_path(), fn, args.get_or("type", ext), args["mode"]);
+    api_mount(resp, fn, args.get_path(), args.get_or("type", ext), args["mode"]);
 }
