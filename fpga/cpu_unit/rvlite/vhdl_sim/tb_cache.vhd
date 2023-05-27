367239e08ef8c2730913871fc880ac45160dc3a0
GideonZ <gideon.zweijtzer@gmail.com>
2023-05-28 00:02:29 +0200

    More thorough testing of the fetch unit and cache.

--- fpga/cpu_unit/rvlite/vhdl_sim/tb_cache.vhd
+++ fpga/cpu_unit/rvlite/vhdl_sim/tb_cache.vhd
@@ -10,24 +10,28 @@ use work.tl_string_util_pkg.all;
 library std;
 use std.textio.all;
 
-entity tb_fetch is
+entity tb_cache is
 
 end entity;
 
-architecture run of tb_fetch is
-    signal imem_o       : imem_out_type;
-    signal imem_i       : imem_in_type;
+architecture run of tb_cache is
+    signal cimem_o      : imem_out_type;
+    signal cimem_i      : imem_in_type;
+    signal imem_o       : dmem_out_type;
+    signal imem_i       : dmem_in_type;
     signal fetch_i      : t_fetch_in;
     signal fetch_o      : t_fetch_out;
     signal rst_i        : std_logic;
     signal clk_i        : std_logic := '0';
     signal rdy_i        : std_logic := '0';
-    signal rdy_interval  : natural := 5;
     signal imem_interval : natural := 5;
-    signal ack          : std_logic := '0';
-    signal expected     : natural := 0;
+    signal expected     : std_logic_vector(31 downto 0) := (others => '0');
     signal wrong        : std_logic := '0';
     constant rdy_pattern : std_logic_vector := "01111110111110111101110110100000100001000100101111100000";
+    type t_targets    is array(natural range <>) of std_logic_vector(31 downto 0);
+
+    constant c_targets : t_targets := (
+        X"00000000", X"00001000", X"00002000", X"00000000", X"00000FF0", X"00001FF0", X"00000030" );
 
 begin
     clk_i <= not clk_i after 10 ns;
@@ -40,33 +44,29 @@ begin
     port map (
         fetch_o => fetch_o,
         fetch_i => fetch_i,
-        imem_o  => imem_o,
-        imem_i  => imem_i,
+        imem_o  => cimem_o,
+        imem_i  => cimem_i,
         rst_i   => rst_i,
         rdy_i   => rdy_i,
         clk_i   => clk_i
     );
 
-    fetch_i.branch_target <= X"00000000";
-    
-    -- -- generate rdy
-    -- process
-    -- begin
-    --     ack <= '0';
-    --     if fetch_o.inst_valid = '0' then
-    --         wait until fetch_o.inst_valid = '1';
-    --     end if;
-    --     for i in 0 to rdy_interval-1 loop
-    --         wait until clk_i='1';
-    --     end loop;
-    --     ack <= '1';
-    --     wait until clk_i='1';
-    -- end process;
-    -- rdy_i <= '1';--not fetch_o.inst_valid or ack;
+    i_cache: entity work.icache
+      port map (
+        clock   => clk_i,
+        reset   => rst_i,
+        disable => '0',
+        dmem_i  => cimem_o,
+        dmem_o  => cimem_i,
+        mem_o   => imem_o,
+        mem_i   => imem_i
+      );
+
+
     process
     begin
         for i in rdy_pattern'range loop
-            rdy_i <= rdy_pattern(i);
+            rdy_i <= '1'; --rdy_pattern(i);
             wait until clk_i='1';
         end loop;
     end process;
@@ -74,26 +74,26 @@ begin
     -- generate jump
     process
     begin 
-        for j in 1 to 10 loop
-            fetch_i.branch <= '0';
-            for i in 1 to 50+j loop 
-                wait until clk_i = '1';
+        for k in c_targets'range loop
+            for m in 0 to 5 loop
+                imem_interval <= m;
+                for j in 1 to 10 loop
+                    fetch_i.branch <= '0';
+                    fetch_i.branch_target <= (others => 'X');
+                    for i in 1 to 50+j loop 
+                        wait until clk_i = '1';
+                    end loop;
+                    fetch_i.branch <= '1';
+                    fetch_i.branch_target <= c_targets(k);
+                    wait until clk_i = '1';
+                end loop;
             end loop;
-            fetch_i.branch <= '1';
-            wait until clk_i = '1';
         end loop;
-        if rdy_interval = 0 then
-            rdy_interval <= 5;
-            imem_interval <= imem_interval - 1;
-        else
-            rdy_interval <= rdy_interval - 1;
-        end if;
     end process;
-
 
     -- memory and IO
     process(clk_i)
-        variable imem_l     : imem_out_type;
+        variable imem_l     : dmem_out_type;
         variable imem_count : natural;
     begin
         if rising_edge(clk_i) then
@@ -121,16 +121,16 @@ begin
     begin
         if rising_edge(clk_i) then
             if rdy_i = '1' and fetch_o.inst_valid = '1' then
-                if fetch_o.program_counter /= std_logic_vector(to_unsigned(expected, 32)) then
+                if fetch_o.program_counter /= expected then
                     wrong <= '1';
                 end if;
-                if fetch_o.instruction /= std_logic_vector(to_unsigned(expected, 32)) then
+                if fetch_o.instruction /= expected then
                     wrong <= '1';
                 end if;
-                expected <= expected + 4;
+                expected <= std_logic_vector(unsigned(expected) + 4);
             end if;
             if fetch_i.branch = '1' then
-                expected <= 0;
+                expected <= fetch_i.branch_target;
             end if;
         end if;
     end process;
