27e76b47b3a64583b147a73738213718929c02b2
GideonZ <gideon.zweijtzer@gmail.com>
2023-05-30 22:46:39 +0200

    Pending change for software iec printer. (To be overruled later.)

--- software/io/iec/iec.cc
+++ software/io/iec/iec.cc
@@ -396,18 +396,14 @@ void IecInterface :: poll()
     	        delete[] pathstring;
     	    }
     	}
-		uint8_t a;
-        int looplimit = 500;
-        while (!((a = HW_IEC_RX_FIFO_STATUS) & IEC_FIFO_EMPTY)) {
-
-            if ((--looplimit) < 0) {
-                printf("-> ** IEC Processor seems to have gone haywire. Stopped! **\n");
-                iec_enable = 0;
-                HW_IEC_RESET_ENABLE = 0;
+        uint8_t a;
+        for (int loopcnt = 0; loopcnt < 500; loopcnt++) {
+            a = HW_IEC_RX_FIFO_STATUS;
+            if (a & IEC_FIFO_EMPTY) {
                 break;
             }
 
-			data = HW_IEC_RX_DATA;
+            data = HW_IEC_RX_DATA;
 			if(a & IEC_FIFO_CTRL) {
 
 #if IECDEBUG > 2
@@ -489,9 +485,9 @@ void IecInterface :: poll()
 			if (wait_irq) {
 				break;
 			}
-		}
+        }
 
-		int st;
+        int st;
 		if(talking) {
 		    while(!(HW_IEC_TX_FIFO_STATUS & IEC_FIFO_FULL)) {
 				st = channels[current_channel]->prefetch_data(data);
