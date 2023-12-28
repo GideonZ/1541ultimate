f220dc3a9f80ddd16c66314c15af905095ac5bb1
GideonZ <gideon.zweijtzer@gmail.com>
2023-12-28 11:35:40 +0100

    Choice to always overwrite index.html

--- software/application/update_u2p/update_common.h
+++ software/application/update_u2p/update_common.h
@@ -113,7 +113,7 @@ static FRESULT write_html_file(const char *name, const char *data, int length)
     File *f;
     uint32_t dummy;
     FileManager *fm = FileManager :: getFileManager();
-    FRESULT fres = fm->fopen(HTML_DIRECTORY, name, FA_CREATE_NEW | FA_WRITE, &f);
+    FRESULT fres = fm->fopen(HTML_DIRECTORY, name, FA_CREATE_ALWAYS | FA_WRITE, &f);
     if (fres == FR_OK) {
         fres = f->write(data, length, &dummy);
         console_print(screen, "Writing %s to /flash: %s\n", name, FileSystem :: get_error_string(fres));
