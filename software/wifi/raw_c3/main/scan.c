68fc6b12042e360e666e94ebe3c8055893327775
GideonZ <gideon.zweijtzer@gmail.com>
2024-03-05 17:41:26 +0100

    Added ESP32-C3 code for WiFi. Mostly a copy of the ESP32 code, so this should be refactored.

---
+++ software/wifi/raw_c3/main/scan.c
@@ -0,0 +1,271 @@
+/* Scan Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/*
+    This example shows how to use the All Channel Scan or Fast Scan to connect
+    to a Wi-Fi network.
+
+    In the Fast Scan mode, the scan will stop as soon as the first network matching
+    the SSID is found. In this mode, an application can set threshold for the
+    authentication mode and the Signal strength. Networks that do not meet the
+    threshold requirements will be ignored.
+
+    In the All Channel Scan mode, the scan will end only after all the channels
+    are scanned, and connection will start with the best network. The networks
+    can be sorted based on Authentication Mode or Signal Strength. The priority
+    for the Authentication mode is:  WPA2 > WPA > WEP > Open
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_system.h"
+#include "my_uart.h"
+#include "dump_hex.h"
+#include "lwip/sockets.h"
+#include "lwip/netdb.h"
+#include "esp_vfs_eventfd.h"
+#include "../lwip/esp_netif_lwip_internal.h"
+#include "driver/gpio.h"
+#include "soc/io_mux_reg.h"
+
+#include "rpc_calls.h"
+#include "rpc_dispatch.h"
+
+#define GPIO_LED  GPIO_NUM_6
+
+#define PIN_UART_RXD 20
+#define PIN_UART_TXD 21
+#define PIN_UART_RTS 4
+#define PIN_UART_CTS 5
+#define PIN_JTAG_TCK 8
+#define PIN_JTAG_TMS 0
+#define PIN_JTAG_TDI 1
+#define PIN_JTAG_TDO 7
+
+#define NET_CMD_BUFSIZE 512
+
+static const char *TAG = "raw_bridge";
+
+// Allocate some buffers to work with
+command_buf_context_t work_buffers;
+
+// do not put it on the stack
+wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+
+esp_netif_t *my_sta_netif = NULL;
+
+// typedef err_t (*netif_input_fn)(struct pbuf *p, struct netif *inp);
+
+static err_t hacked_recv(struct pbuf *p, struct netif *inp)
+{
+    static uint16_t seq = 0;
+    command_buf_t *reply;
+    if ((memcmp(p->payload, inp->hwaddr, 6) == 0) || 
+        (memcmp(p->payload, "\xff\xff\xff\xff\xff\xff", 6) == 0)) {
+
+        if (my_uart_get_buffer(UART_NUM_1, &reply, 100)) {
+            rpc_rx_pkt *pkt = (rpc_rx_pkt *)reply->data;
+            pkt->hdr.command = EVENT_RECV_PACKET;
+            pkt->hdr.thread = 0xFF;
+            pkt->hdr.sequence = seq++;
+            pkt->len = p->len;
+            memcpy(&pkt->data, p->payload, p->len);
+            reply->size = sizeof(rpc_rx_pkt) + p->len - 1;
+            pbuf_free(p);
+            my_uart_transmit_packet(UART_NUM_1, reply);
+            return ERR_OK;
+        } else {
+            ESP_LOGW(TAG, "No buffer to send packet.");
+            return ERR_BUF;
+        }
+    } else { // dropped, wrong mac and no broadcast
+        pbuf_free(p);
+        return ERR_OK;
+    }
+}
+
+static err_t hacked_tx(struct netif *netif, struct pbuf *pbuf)
+{
+    //pbuf_free(pbuf);
+    return ERR_OK;
+}
+
+
+static void got_ip_event_handler(void *esp_netif, esp_event_base_t base, int32_t event_id, void *data)
+{
+    const ip_event_got_ip_t *event = (const ip_event_got_ip_t *) data;
+
+    ESP_LOGI(TAG, "HET IS WAT!! ip: " IPSTR ", mask: " IPSTR ", gw: " IPSTR,
+             IP2STR(&event->ip_info.ip),
+             IP2STR(&event->ip_info.netmask),
+             IP2STR(&event->ip_info.gw));
+
+    command_buf_t *reply;
+    if (my_uart_get_buffer(UART_NUM_1, &reply, 100)) {
+        event_pkt_got_ip *ev = (event_pkt_got_ip *)reply->data;
+        ev->hdr.command = EVENT_GOTIP;
+        ev->hdr.thread = 0xFF;
+        ev->hdr.sequence = 0;
+        ev->ip = event->ip_info.ip.addr;
+        ev->netmask = event->ip_info.netmask.addr;
+        ev->gw = event->ip_info.gw.addr;
+        ev->changed = event->ip_changed;
+        reply->size = sizeof(event_pkt_got_ip);
+        my_uart_transmit_packet(UART_NUM_1, reply);
+    } else {
+        ESP_LOGW(TAG, "No buffer to send event.");
+    }
+}
+
+static void wifi_event_handler(void *esp_netif, esp_event_base_t base, int32_t event_id, void *data)
+{
+    ESP_LOGW(TAG, "WiFi Event %ld, data %p", event_id, data);
+
+    uint8_t evcode = 0;
+    switch(event_id) {
+        case WIFI_EVENT_STA_DISCONNECTED:
+            evcode = EVENT_DISCONNECTED;
+            break;
+        default:
+            break;
+    }
+    struct netif *lw = ((struct esp_netif_obj *)my_sta_netif)->lwip_netif;
+    lw->input = hacked_recv;
+    lw->linkoutput = hacked_tx;
+
+    if (evcode) {
+        command_buf_t *reply;
+        if (my_uart_get_buffer(UART_NUM_1, &reply, 100)) {
+            rpc_header_t *ev = (rpc_header_t *)reply->data;
+            ev->command = evcode;
+            ev->thread = 0xFF;
+            ev->sequence = 0;
+            reply->size = sizeof(rpc_header_t);
+            my_uart_transmit_packet(UART_NUM_1, reply);
+        } else {
+            ESP_LOGW(TAG, "No buffer to send event.");
+        }
+    }
+}
+
+// Initialize Wi-Fi as sta
+static void wifi_init()
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    my_sta_netif = esp_netif_create_default_wifi_sta();
+    assert(my_sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    struct netif *lw = ((struct esp_netif_obj *)my_sta_netif)->lwip_netif;
+    lw->input = hacked_recv;
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &got_ip_event_handler,
+                                                        NULL,
+                                                        NULL));
+}
+
+// Initialize Wi-Fi issue scan command
+esp_err_t wifi_scan(ultimate_ap_records_t *ult_records)
+{
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
+
+    ult_records->reserved = 0;
+    ult_records->num_records = 0;
+
+    esp_err_t err;
+    err = esp_wifi_scan_start(NULL, true);
+    if (err != ESP_OK) {
+        return err;
+    }
+    err = esp_wifi_scan_get_ap_records(&number, ap_info);
+    if (err != ESP_OK) {
+        return err;
+    }
+    err = esp_wifi_scan_get_ap_num(&ap_count);
+    if (err != ESP_OK) {
+        return err;
+    }
+
+    ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
+    if (ap_count > DEFAULT_SCAN_LIST_SIZE) {
+    	ap_count = DEFAULT_SCAN_LIST_SIZE;
+    }
+
+    for (int i = 0; i < ap_count; i++) {
+        memcpy(ult_records->aps[i].bssid, ap_info[i].bssid, 6);
+        memcpy(ult_records->aps[i].ssid, ap_info[i].ssid, 33);
+        ult_records->aps[i].channel = ap_info[i].primary;
+        ult_records->aps[i].rssi = ap_info[i].rssi;
+        ult_records->aps[i].authmode = ap_info[i].authmode;
+    }
+
+    ult_records->num_records = (uint8_t)ap_count;
+    return ESP_OK;
+}
+
+void app_main()
+{
+	// Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    gpio_iomux_out(GPIO_LED, FUNC_MTCK_GPIO6, false);
+    gpio_set_direction(GPIO_LED, GPIO_MODE_OUTPUT);
+
+    ESP_LOGI(TAG, IDENT_STRING);
+    ESP_LOGI(TAG, "disconnect UART 1 pins...");
+    ESP_ERROR_CHECK( uart_set_pin(UART_NUM_1, -1, -1, -1, -1));
+
+    ESP_LOGI(TAG, "cmd_buffer_init...");
+    // Initialize the single copy transport buffers, and attach dispatchers to each buffer
+    cmd_buffer_init(&work_buffers);
+
+    // Initialize Dispatchers and attach them to the buffers
+#if MULTITHREADED
+    create_dispatchers(DISPATCHER_STACK, &work_buffers);
+#endif
+    ESP_LOGI(TAG, "my_uart_init...");
+    // Now install the driver for UART1.  We use our own buffer mechanism and interrupt routine
+    ESP_ERROR_CHECK(my_uart_init(&work_buffers, UART_NUM_1));
+
+    ESP_LOGI(TAG, "wifi_init...");
+    wifi_init();
+
+    ESP_LOGI(TAG, "dispatch... switching UART pins!");
+
+    vTaskDelay(100);
+    ESP_ERROR_CHECK( uart_set_pin(UART_NUM_0, 0, -1, -1, -1));
+    ESP_ERROR_CHECK( uart_set_pin(UART_NUM_1, PIN_UART_TXD, PIN_UART_RXD, PIN_UART_RTS, PIN_UART_CTS));
+    dispatch(work_buffers.receivedQueue);
+}
