c04408e3d0b18126b316d841631fa6b8a22ea06b
GideonZ <gideon.zweijtzer@gmail.com>
2025-05-04 12:20:06 +0200

    Prio bugfix

--- software/network/config/lwipopts.h
+++ software/network/config/lwipopts.h
@@ -839,7 +839,7 @@
  * The priority value itself is platform-dependent, but is passed to
  * sys_thread_new() when the thread is created.
  */
-#define TCPIP_THREAD_PRIO               PRIO_TCPIP_STACK
+#define TCPIP_THREAD_PRIO               PRIO_TCPIP
 
 /**
  * TCPIP_MBOX_SIZE: The mailbox size for the tcpip thread messages
