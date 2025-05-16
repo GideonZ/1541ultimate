524094fcc2555c9a5bd76fd7449dc1a0d0a53839
markusC64 <markusC64@localhost>
2025-05-16 10:35:09 +0200

    WIP: U64 II

--- software/io/rtc/rtc_dummy.cc
+++ software/io/rtc/rtc_dummy.cc
@@ -1,5 +1,7 @@
-#include "rtc_dummy.h"
+#include "rtc_i2c_sntp.h"
 #include <stdio.h>
+#include "u2p.h"
+
 #include <stdlib.h>
 #include <time.h>
 #include "sntp_time.h"
@@ -99,7 +101,31 @@ const char *zone_names[] = {
     "Christmas Island",
 };
 
+// =================
+//   Configuration
+// =================
+
+#define CFG_RTC_YEAR    0x11
+#define CFG_RTC_MONTH   0x12
+#define CFG_RTC_DATE    0x13
+// #define CFG_RTC_WEEKDAY 0x14
+#define CFG_RTC_HOUR    0x15
+#define CFG_RTC_MINUTE  0x16
+#define CFG_RTC_SECOND  0x17
+#define CFG_RTC_CORR    0x18
 #define CFG_RTC_ZONE    0x19
+
+struct t_cfg_definition rtc_config[] = {
+    { CFG_RTC_YEAR,     CFG_TYPE_VALUE,  "Year",     "%d", NULL,  1980, 2079, 2015 },
+    { CFG_RTC_MONTH,    CFG_TYPE_ENUM,   "Month",    "%s", month_strings_long,  1, 12,  10 },
+    { CFG_RTC_DATE,     CFG_TYPE_VALUE,  "Day",      "%d", NULL,     1, 31,  13 },
+//  { CFG_RTC_WEEKDAY,  CFG_TYPE_ENUM,   "Weekday",  "%s", weekday_strings, 0, 6, 2 },
+    { CFG_RTC_HOUR,     CFG_TYPE_VALUE,  "Hours",   "%02d", NULL,     0, 23, 16 },
+    { CFG_RTC_MINUTE,   CFG_TYPE_VALUE,  "Minutes", "%02d", NULL,     0, 59, 52 },
+    { CFG_RTC_SECOND,   CFG_TYPE_VALUE,  "Seconds", "%02d", NULL,     0, 59, 55 },
+    { CFG_RTC_CORR,     CFG_TYPE_VALUE,  "Correction", "%d ppm", NULL, -200, 200, 0 },
+    { CFG_TYPE_END,     CFG_TYPE_END,    "", "", NULL, 0, 0, 0 }
+};
 
 struct t_cfg_definition time_config[] = {
     { CFG_RTC_ZONE,     CFG_TYPE_ENUM,   "TimeZone", "%s", zone_names, 0, (sizeof(zone_names)/sizeof(const char *))-1, 16 },
@@ -108,13 +134,49 @@ struct t_cfg_definition time_config[] = {
 
 Rtc::Rtc()
 {
+    capable = true;
     register_store(0x54696d65, "Time Settings", time_config);
+    cfg2 = new RtcConfigStore("Clock Settings", rtc_config);
+    ConfigManager::getConfigManager()->add_custom_store(cfg2);
 }
 
 Rtc::~Rtc()
 {
+    if (capable) {
+        ConfigManager::getConfigManager()->remove_store(cfg);
+        delete cfg;
+    }
 }
 
+void Rtc::effectuate_settings(void)
+{
+}
+
+void Rtc::write_byte(int addr, uint8_t val)
+{
+}
+
+void Rtc::read_all(void)
+{
+}
+
+bool Rtc::is_valid(void)
+{
+    return true;
+}
+
+void Rtc::get_time_from_chip(void)
+{
+}
+
+void Rtc::set_time_in_chip(int corr_ppm, int y, int M, int D, int wd, int h, int m, int s)
+{
+}
+
+int Rtc::get_correction(void)
+{
+	return 0;
+}
 
 void Rtc::get_time(int &y, int &M, int &D, int &wd, int &h, int &m, int &s)
 {
@@ -136,6 +198,20 @@ void Rtc::get_time(int &y, int &M, int &D, int &wd, int &h, int &m, int &s)
     h = timeinfo.tm_hour;
     m = timeinfo.tm_min;
     s = timeinfo.tm_sec;
+}
+
+void Rtc::set_time(int y, int M, int D, int wd, int h, int m, int s)
+{
+    struct tm timeinfo;
+    timeinfo.tm_year = y + 80;
+    timeinfo.tm_mon = M - 1;
+    timeinfo.tm_mday = D;
+    timeinfo.tm_wday = wd;
+    timeinfo.tm_hour = h;
+    timeinfo.tm_min = m;
+    timeinfo.tm_sec = s;
+    time_t seconds = mktime(&timeinfo);
+    RTC_TIMER_SECONDS = seconds;
 }
 
 const char * Rtc::get_time_string(char *dest, int len)
@@ -171,6 +247,9 @@ const char * Rtc::get_long_date(char *dest, int len)
 
 uint32_t Rtc::get_fat_time(void)
 {
+    if (!capable)
+        return 0x42FF9877;
+
     int y, M, D, wd, h, m, s;
     get_time(y, M, D, wd, h, m, s);
 
@@ -185,30 +264,123 @@ uint32_t Rtc::get_fat_time(void)
     return result;
 }
 
-int Rtc::get_correction(void)
-{
-    return 0;
-}
-
-void Rtc::set_time_in_chip(int corr_ppm, int y, int M, int D, int wd, int h, int m, int s)
-{
-}
-
-void Rtc::set_time(int y, int M, int D, int wd, int h, int m, int s)
-{
-    struct tm timeinfo;
-    timeinfo.tm_year = y + 80;
-    timeinfo.tm_mon = M - 1;
-    timeinfo.tm_mday = D;
-    timeinfo.tm_wday = wd;
-    timeinfo.tm_hour = h;
-    timeinfo.tm_min = m;
-    timeinfo.tm_sec = s;
-    time_t seconds = mktime(&timeinfo);
-    RTC_TIMER_SECONDS = seconds;
-}
-
 Rtc rtc; // global
+
+// ============================================================
+// == Functions that link the RTC to the configuration manager
+// ============================================================
+void RtcConfigStore::at_open_config(void)
+{
+    printf("** Cfg RTC Read **\n");
+    int y, M, D, wd, h, m, s;
+    rtc.get_time(y, M, D, wd, h, m, s);
+    int corr = rtc.get_correction();
+    ConfigItem *i;
+
+    for (int n = 0; n < items.get_elements(); n++) {
+        i = items[n];
+        switch (i->definition->id) {
+        case CFG_RTC_YEAR:
+            i->value = 1980 + y;
+            break;
+        case CFG_RTC_MONTH:
+            i->value = M;
+            break;
+        case CFG_RTC_DATE:
+            i->value = D;
+            break;
+            /*
+             case CFG_RTC_WEEKDAY:
+             i->value = wd;
+             break;
+             */
+        case CFG_RTC_HOUR:
+            i->value = h;
+            break;
+        case CFG_RTC_MINUTE:
+            i->value = m;
+            break;
+        case CFG_RTC_SECOND:
+            i->value = s;
+            break;
+        case CFG_RTC_CORR:
+            i->value = corr;
+            break;
+        default:
+            break;
+        }
+    }
+    check_bounds();
+}
+
+void RtcConfigStore::at_close_config(void)
+{
+    printf("** Cfg RTC Write **\n");
+
+    if (!need_effectuate())
+        return;
+
+    
+
+    int y, M, D, wd, h, m, s, corr;
+
+    ConfigItem *i;
+
+    for (int n = 0; n < items.get_elements(); n++) {
+        i = items[n];
+        int value = i->getValue();
+        switch (i->definition->id) {
+        case CFG_RTC_YEAR:
+            y = value - 1980;
+            break;
+        case CFG_RTC_MONTH:
+            M = value;
+            break;
+        case CFG_RTC_DATE:
+            D = value;
+            break;
+            /*
+             case CFG_RTC_WEEKDAY:
+             wd = i->value;
+             break;
+             */
+        case CFG_RTC_HOUR:
+            h = value;
+            break;
+        case CFG_RTC_MINUTE:
+            m = value;
+            break;
+        case CFG_RTC_SECOND:
+            s = value;
+            break;
+        case CFG_RTC_CORR:
+            corr = value;
+            break;
+        default:
+            break;
+        }
+    }
+
+    int yz, mz, cz;
+    mz = M;
+    yz = y + 1980;
+    if (mz < 3) {
+        yz--;
+        mz += 12;
+    }
+    cz = yz / 100;
+    yz = yz % 100;
+    wd = D + (mz + 1) * 13 / 5 + yz + yz / 4 + cz / 4 - 2 * cz + 6;
+    wd = wd % 7;
+    if (wd < 0)
+        wd += 7;
+
+    printf("Writing time: %d-%d-%d (%d) %02d:%02d:%02d\n", y, M, D, wd, h, m, s);
+    rtc.set_time(y, M, D, wd, h, m, s);
+    rtc.set_time_in_chip(corr, y, M, D, wd, h, m, s);
+
+    set_effectuated();
+}
 
 extern "C" uint32_t get_fattime(void) /* 31-25: Year(0-127 org.1980), 24-21: Month(1-12), 20-16: Day(1-31) */
 /* 15-11: Hour(0-23), 10-5: Minute(0-59), 4-0: Second(0-29 *2) */
