809f6fac9b01eb3a58faa63d010b47d2068cef7b
GideonZ <gideon.zweijtzer@gmail.com>
2026-03-11 22:02:41 +0100

    Both KB and strip LED intensities default to 28.

--- software/u64/bling_board.cc
+++ software/u64/bling_board.cc
@@ -44,7 +44,7 @@ static struct t_cfg_definition cfg_definition[] = {
     { CFG_LED_AUTOSID,          CFG_TYPE_ENUM,  "LedStrip Auto SID Mode",       "%s", en_dis,       0,  1,  1  },
     { CFG_LED_PATTERN,          CFG_TYPE_ENUM,  "LedStrip Pattern",             "%s", patterns,     0,  4,  0  },
     { CFG_LED_SIDSELECT,        CFG_TYPE_ENUM,  "LedStrip SID Select",          "%s", sidsel,       0,  7,  0  },
-    { CFG_LED_INTENSITY,        CFG_TYPE_VALUE, "Strip Intensity",              "%d", NULL,         0, 31,  6  },
+    { CFG_LED_INTENSITY,        CFG_TYPE_VALUE, "Strip Intensity",              "%d", NULL,         0, 31, 28  },
     { CFG_LED_FIXED_COLOR,      CFG_TYPE_ENUM,  "Fixed Color",                  "%s", fixed_colors, 0, 24, 15  },
     { CFG_LED_FIXED_TINT,       CFG_TYPE_ENUM,  "Color tint",                   "%s", color_tints,  0,  3,  0  },
     { CFG_TYPE_END,             CFG_TYPE_END,    "", "", NULL, 0, 0, 0 }
