a0a869acf2b5a6d54e9efcb19ca2ed99d7357864
GideonZ <gideon.zweijtzer@gmail.com>
2026-06-20 22:14:57 +0200

    Bug fixes in memory allocation using FreeRTOS allocator.

--- software/system/memory_wrap.cc
+++ software/system/memory_wrap.cc
@@ -1,6 +1,7 @@
 #include "FreeRTOS.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void * get_mem(size_t size) 
 {
@@ -68,29 +69,51 @@ extern "C" {
         vPortFree(ptr);
     }
 
+    void *__wrap_calloc(size_t nmemb, size_t size) {
+        size_t actual = nmemb * size;
+        void *mem = get_mem(actual);
+        if (mem) {
+            memset(mem, 0, actual);
+        }
+        return mem;
+    }
+
+    void *__wrap__calloc_r(struct _reent *r, size_t nmemb, size_t size) {
+        (void)r; // We ignore Newlib's reentrancy struct because FreeRTOS handles it
+        size_t actual = nmemb * size;
+        void *mem = get_mem(actual);
+        if (mem) {
+            memset(mem, 0, actual);
+        }
+        return mem;
+    }
+
     // And for realloc...
-    void *__wrap_realloc(void* ptr, size_t size) {
+    void *__wrap_realloc(void *ptr, size_t size)
+    {
         // FreeRTOS doesn't have a native realloc,
         // so we manually handle the logic to stay safe.
-        vPortFree(ptr);
+        if (ptr == NULL)
+            return (size ? pvPortMalloc(size) : NULL);
+
         if (size == 0) {
-            return nullptr;
+            vPortFree(ptr);
+            return NULL;
         }
-        
-        return pvPortMalloc(size);
+
+        // heap_4: block size (incl. header, with the top "allocated" bit) sits at ptr-4
+        size_t hdr = ((size_t *)ptr)[-1] & ~((size_t)1 << 31);
+        size_t old_usable = hdr - 8 /* xHeapStructSize */;
+
+        void *nw = pvPortMalloc(size);
+        memcpy(nw, ptr, (old_usable < size) ? old_usable : size);
+        vPortFree(ptr);
+        return nw;
     }
 
     void *__wrap__realloc_r(struct _reent *r, void* ptr, size_t size)
     {
         (void)r;
-
-        // FreeRTOS doesn't have a native realloc,
-        // so we manually handle the logic to stay safe.
-        vPortFree(ptr);
-        if (size == 0) {
-            return nullptr;
-        }
-        
-        return pvPortMalloc(size);
+        return __wrap_realloc(ptr, size);
     }
 }
