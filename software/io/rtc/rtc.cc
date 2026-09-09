ec195d4bb867c30b88bacbf95f1435c2f32d7f4f
Christian Gleissner <chrisgleissner@gmail.com>
2026-09-09 10:02:30 +0100

    fix(rtc): name the year the clock counts from
    Four copies of get_current_time added the same literal.

--- software/io/rtc/rtc.cc
+++ software/io/rtc/rtc.cc
@@ -1,4 +1,5 @@
 #include "rtc.h"
+#include "rtc_epoch.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -419,10 +420,7 @@ extern "C" uint32_t get_fattime(void) /* 31-25: Year(0-127 org.1980), 24-21: Mon
 extern "C" void get_current_time(int& wd, int& year, int& month, int& day, int& hour, int& min, int& sec)
 {
     rtc.get_time(year, month, day, wd, hour, min, sec);
-    // The chip counts years from 1980, the way a FAT time stamp does. Callers of
-    // this function want a calendar year: the drive's own time commands print it
-    // straight into the answer they give the Commodore.
-    year += 1980;
+    year += RTC_EPOCH_YEAR; // get_time() counts years from the epoch, callers do not
 }
 
 #include "init_function.h"
--- software/io/rtc/rtc_only_i2c.cc
+++ software/io/rtc/rtc_only_i2c.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "rtc_only.h"
+#include "rtc_epoch.h"
 #include "i2c_drv.h"
 #include "itu.h"
 
@@ -224,10 +225,7 @@ uint32_t get_fattime (void)	/* 31-25: Year(0-127 org.1980), 24-21: Month(1-12), 
 extern "C" void get_current_time(int& wd, int& year, int& month, int& day, int& hour, int& min, int& sec)
 {
     rtc.get_time(year, month, day, wd, hour, min, sec);
-    // The chip counts years from 1980, the way a FAT time stamp does. Callers of
-    // this function want a calendar year: the drive's own time commands print it
-    // straight into the answer they give the Commodore.
-    year += 1980;
+    year += RTC_EPOCH_YEAR; // get_time() counts years from the epoch, callers do not
 }
 
 
